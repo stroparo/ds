70e3405b2e59fa5815f347a76ec9e0517afc8de3
Cristian Stroparo <cstropz@gmail.com>
2016-01-30 22:43:09 -0200

    Added gitconfig and gitclones functions; Renamed gpushall function to gpall.

--- env-devel.sh
+++ env-devel.sh
@@ -6,7 +6,10 @@
 # ##############################################################################
 # Devel functions
 
-# Function gg: exec git for all repos descending of current directory.
+# ##############################################################################
+# Git
+
+# Function gg - exec git for all repos descending from current directory.
 # Syntax: [-c command] [command subcommands arguments etc.]
 gg () {
     typeset gitcmd='git'
@@ -33,18 +36,60 @@ EOF
     unset gitcmd
 }
 
-# Function gpushall: pushes current directory repo to all of its remotes.
-unset gpushall
-gpushall () {
+# Function gitclones - Clones the repos passed as the first argument, one per
+#  line in that argument (use quotes).
+# Syntax: {repositories-one-per-line}
+gitclones () {
+    while read repo ; do
+        if [ ! -d "$(basename "${repo%.git}")" ] && ! git clone "${repo}" ; then
+            echo "Failed cloning '${repo}' repository. Aborted sequence." 1>&2
+            return 1
+        fi
+        echo '' 1>&2
+    done <<EOF
+${1}
+EOF
+}
+
+# Function gitconfig - Configures ssh key if there is none, and setup git config.
+# Syntax: {email} {name} [other git config --global options]
+# Example: gitconfig "john@doe.com" "John Doe" 'core.autocrlf false' 'push.default simple'
+gitconfig () {
+    typeset email="${1}"
+    typeset name="${2}"
+    shift 2
+
+    [ -z "${email}" -o "${name}" ] && 'Aborted. Must pass email and name.' 1>&2 && return 1
+
+    # Create an ssh key if there is none:
+    [ ! -e "${HOME}"/.ssh/id_rsa ] && ssh-keygen -t rsa -b 4096 -C "${email}"
+
+    git config --global user.name "${name}"
+    git config --global user.email "${email}"
+
+    for i in "$@" ; do
+        git config --global ${1}
+        shift
+    done
+}
+
+# Function gpall: pushes current directory repo to all of its remotes.
+unset gpall
+gpall () {
     for i in $(git remote); do
         echo "=> Pushing to remote ${i}.."
         git push "${i}" && echo "=> Pushed to remote ${i}."
     done
 }
 
+# ##############################################################################
+# PostgreSQL
+
 # Function supg: call psql via su - postgres, at the given port and user.
 # Syntax: [port=5432] [user=postgres]
 supg () {
     sudo su - postgres -c "psql -p ${1:-5432} -U ${2:-postgres}"
 }
+
+# ##############################################################################
 
