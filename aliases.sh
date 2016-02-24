6869db0e126b56b911a3715fec393d470b2068c2
Cristian Stroparo <cstropz@gmail.com>
2016-02-24 01:06:57 -0300

    aliases new psu ps-ef|grep $USER.

--- aliases.sh
+++ aliases.sh
@@ -23,6 +23,7 @@ alias psfe='ps -fe'
 alias psfens='ps -fe | grep -v bash | grep -v sshd'
 alias psfu='ps -fu "${UID:-$(id -u)}" -U "${UID:-$(id -u)}"'
 alias psfuns='ps -fu "${UID:-$(id -u)}" -U "${UID:-$(id -u)}" | grep -v bash | grep -v sshd'
+alias psu='ps -ef|grep "${USER}"'
 alias psuu='ps -u "${UID:-$(id -u)}" -U "${UID:-$(id -u)}" u'
 alias psuuns='ps -u "${UID:-$(id -u)}" -U "${UID:-$(id -u)}" u | grep -v bash | grep -v sshd'
 
