955b2ddd3ce1451c9811151fdacccc83c1915de6
Cristian Stroparo <cstropz@gmail.com>
2016-03-02 21:30:33 -0300

    Merged env-tests into dsbase.

--- dsbase.sh
+++ dsbase.sh
@@ -645,4 +645,42 @@ printawk () {
 }
 
 # ##############################################################################
+# Testing functions
+
+_all_not_null () {
+    for i in "$@" ; do
+        [ -z "${i}" ] && return 1
+    done
+    return 0
+}
+
+_any_dir_not_w () {
+    for i in "$@" ; do
+        [ ! -d "${1}" -o ! -w "${1}" ] && return 0
+    done
+    return 1
+}
+
+_any_exists () {
+    for i in "$@" ; do
+        [ -e "${1}" ] && return 0
+    done
+    return 1
+}
+
+_any_null () {
+    for i in "$@" ; do
+        [ -z "${i}" ] && return 0
+    done
+    return 1
+}
+
+_any_not_w () {
+    for i in "$@" ; do
+        [ -n "${1}" -a ! -w "${1}" ] && return 0
+    done
+    return 1
+}
+
+# ##############################################################################
 
