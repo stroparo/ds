e6acee4474dd363cfc7ca7b07dbb8a6a0ab69165
Cristian Stroparo <cstropz@gmail.com>
2016-04-13 21:56:07 -0300

    Renamed cleanubuntu to aptclean, added new ckaptitude and fixaptmodes functions.

--- env-admin.sh
+++ env-admin.sh
@@ -47,12 +47,70 @@ screenk () {
 # ##############################################################################
 # Debian
 
+# Function ckaptitude - checks and installs aptitude if unavailable.
+unset ckaptitude
+ckaptitude () {
+    typeset pname=ckaptitude
+
+    if ! which aptitude > /dev/null 2>&1 ; then
+        elog -n "$pname" 'Installing aptitude..'
+
+        sudo apt-get update \
+        && sudo apt-get install -y aptitude 
+
+        if ! which aptitude > /dev/null 2>&1 ; then
+            elog -n "$pname" -f 'Failed installing aptitude. Aborted'
+            return 1
+        fi
+        elog -n "$pname" 'Done installing aptitude.'
+    fi
+}
+
+# Function aptclean - clean up ubuntu packages and unwanted files.
+# Rmk - this also installs localepurge, but it must be executed separately (in that
+#   package you will choose only the locales you use and/or want to keep).
+unset aptclean
+aptclean () {
+    typeset pname=aptclean
+    typeset rmorphan
+
+    elog -n "$pname" 'Started.'
+
+    ckaptitude || return 1
+    sudo aptitude update || return 2
+
+    which deborphan > /dev/null 2>&1 || sudo aptitude install -y deborphan
+    which localepurge > /dev/null 2>&1 || sudo aptitude install -y localepurge
+
+    # Remove bulky stock packages:
+    sudo aptitude purge -y oxygen-icon-theme
+
+    # Remove orphaned packages:
+    if which deborphan > /dev/null 2>&1 ; then
+        elog -n "$pname" '...'
+        elog -n "$pname" 'Orphaned packages:'
+        sudo deborphan
+        elog -n "$pname" 'Remove? (y|n) '
+        read rmorphan
+        if [[ ${rmorphan} = y* ]] ; then
+            sudo deborphan | xargs sudo apt-get purge -y
+        fi
+    fi
+
+    # Remove caches:
+    sudo apt-get autoclean -y
+    sudo apt-get clean -y
+
+    elog -n "$pname" 'Completed.'
+}
+
 # Function aptinstall - Install packages listed in file.
 # Syntax: filename
 unset aptinstall
 aptinstall () {
+    typeset assumeyes doupgrade pkgslist
 
-    while getopts ':u' option ; do
+    while getopts ':uy' option ; do
         case "${option}" in
         u) doupgrade=true;;
         y) assumeyes='-y';;
@@ -64,15 +122,14 @@ aptinstall () {
         echo "A readable packagelist file must be passed as the first argument. Aborted." 1>&2
         return 1
     fi
-  
-    typeset pkgslist=$(sed -e 's/#.*$//' "${1}" | grep .)
-  
+    pkgslist=$(sed -e 's/#.*$//' "${1}" | grep .)
+
+    ckaptitude || return 1
     sudo aptitude update || return 2
   
     if ${doupgrade:-false} ; then
         sudo aptitude upgrade ${assumeyes} || return 11
     fi
-  
     sudo aptitude install ${assumeyes} -Z ${pkgslist} || return 21
 }
 
@@ -92,6 +149,16 @@ dpkgstat () {
         /^Status:/ {
             stat = $0; printf("%-32s%s\n", pkg, stat);
         }'
+}
+
+# Function fixaptmodes - Fix workaround for /etc/apt/sources.list.d mode issue.
+#   This will sudo chmod 644 to all files in /etc/apt/sources.list.d
+# Rmk: Common scenario this glitch happens is a call to update after adding a ppa repo.
+unset fixaptmodes
+fixaptmodes () {
+    if [ -d /etc/apt/sources.list.d ] ; then
+        sudo chmod 644 /etc/apt/sources.list.d/*
+    fi
 }
 
 # ##############################################################################
@@ -260,47 +327,6 @@ atexit.register(save_history)
 del os, atexit, readline, rlcompleter, save_history, historyPath
 EOF
 
-}
-
-# ##############################################################################
-# Ubuntu
-
-# Function cleanubuntu - clean up ubuntu packages and unwanted files.
-# Rmk - this also installs localepurge, but it must be executed separately (in that
-#   package you will choose only the locales you use and/or want to keep).
-unset cleanubuntu
-cleanubuntu () {
-    typeset pname=cleanubuntu
-    typeset rmorphan
-
-    elog -n "$pname" 'Started.'
-
-    if _is_ubuntu ; then
-        sudo apt-get update
-
-        sudo apt-get install -y deborphan localepurge
-
-        # Remove bulky stock packages:
-        sudo apt-get purge --auto-remove oxygen-icon-theme
-
-        # Remove caches:
-        sudo apt-get autoclean -y
-        sudo apt-get clean -y
-
-        # Remove orphaned packages:
-        elog -n "$pname" '...'
-        elog -n "$pname" 'Orphaned packages:'
-        sudo deborphan
-        elog -n "$pname" 'Remove? (y|n) '
-        read rmorphan
-        if [[ ${rmorphan} = y* ]] ; then
-            sudo deborphan | xargs sudo apt-get purge -y
-        fi
-    else
-        elog -n "$pname" -s "Not in Ubuntu."
-        return 1
-    fi
-    elog -n "$pname" 'Completed.'
 }
 
 # ##############################################################################
