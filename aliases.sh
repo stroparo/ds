390ae6daa238380a77f3ea5ac919d3f343f9090f
Cristian Stroparo <cstropz@gmail.com>
2016-04-17 00:09:29 -0300

    Git aliases to replicate those of Oh My ZSH into bash.

--- aliases.sh
+++ aliases.sh
@@ -61,3 +61,32 @@ else
     alias lt='ls -Flrt'
 fi
 
+# Aliases - dev - git:
+alias gh='git diff HEAD'
+alias gv='git mv'
+if [ -n "${BASH_VERSION}" ] ; then
+    # Replicate Oh My ZSH Git aliases in Bash:
+    alias ga='git add'
+    alias gb='git branch'
+    alias gc='git commit -v'
+    alias gco='git checkout'
+    alias gd='git diff'
+    alias gdca='git diff --cached'
+    alias gl='git pull'
+    alias glog='git log --oneline --decorate --graph'
+    alias gp='git push'
+    alias grh='git reset HEAD'
+    alias grhh='git reset HEAD --hard'
+    alias gru='git reset --'
+    alias gss='git status -s'
+    alias gst='git status'
+    alias gts='git tag -s'
+
+    alias gr='git remote'
+    alias gra='git remote add'
+    alias grmv='git remote rename'
+    alias grrm='git remote remove'
+    alias grset='git remote set-url'
+    alias grup='git remote update'
+    alias grv='git remote -v'
+fi
