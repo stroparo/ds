885a55c5ca01ebd6bd2efebf69e73e35c7779dc0
Cristian Stroparo <cstropz@gmail.com>
2016-05-17 21:45:43 -0300

    Enter environment ee* functions incorporated to env-admin as Remote - Shell (ops) section.

--- env-admin.sh
+++ env-admin.sh
@@ -343,6 +343,119 @@ EOF
 }
 
 # ##############################################################################
+# Remote - Shell
+# EE - Enter-Environment routines for ssh and scp operations
+
+# Globals:
+# EEPATH (export):
+#  Directories containing (recursively) ee.txt files to load entries from,
+#  each entry syntax being:
+#
+# [entry]
+# attribute=value
+#
+# Mandatory attributes:
+# ee_desc='description'
+# ee_user=user
+# ee_host=hostname
+#
+# Optional attributes:
+# ee_cmd='some command'
+# ee_id='some .pem or other file to be handled to ssh -i option'
+
+alias eep='scp ${ee_id:+ -i "${ee_id}"}'
+
+# Enter environment list available in EEPATH ee.txt files:
+eel () {
+    while IFS=: read eepath ; do
+        # Search for the entry in EEPATH ee.txt files and setup variables if found:
+        while read eefile ; do
+            echo "==> '${eefile}' <==" 1>&2
+            awk '/^ *\[.*\] *$/ { gsub(/[][]/, ""); print; }' "${eefile}"
+        done <<EOF
+$(find "${eepath}" -type f -name 'ee.txt')
+EOF
+    done <<EOF
+${EEPATH}
+EOF
+}
+
+# Enter environment execute ie connect to the environment. System command is ssh.
+eex () {
+    if test -n "${ee_cmd}" && test -z "${1}" ; then
+        echo 'WARN: There is ee_cmd set but this eex call has no arguments.' 1>&2
+    fi
+
+    if [ -n "${ee_id}" ] ; then
+        ssh -i "${ee_id}" -l "${ee_user}" "${ee_host}" "$@"
+    else
+        ssh -l "${ee_user}" "${ee_host}" "$@"
+    fi
+}
+
+# Function ees - Enter-Environment select environment (sets up env. variables).
+ee () {
+    typeset ee_name_search eefile eepath selectonly
+    ee_name=""; ee_desc=""; ee_user=""; ee_host=""; ee_domain=""; ee_id=""; ee_cmd=""
+
+    # Option parsing:
+    while getopts ':s' opt ; do
+        case "${opt}" in
+        s) selectonly=true ;;
+        esac
+    done
+    shift $((OPTIND-1)) ; OPTIND=1
+
+    ee_name_search="$1"
+    shift
+
+    if [ -z "${ee_name_search}" ] ; then
+        echo 'FAIL: Must pass a nonempty environment name/id.' 1>&2
+        return 1
+    fi
+
+    while IFS=: read eepath ; do
+        # Search for the entry in EEPATH ee.txt files and setup variables if found:
+        while read eefile ; do
+            eval "$(awk -vee_name_search="${ee_name_search}" '
+
+            # Find the entry:
+            /^ *\['"${ee_name_search}"'\] *$/ { found = 1; print "ee_name=" ee_name_search; }
+
+            # Print entry content:
+            found && $0 ~ /^ *[^[]/ { inbody = 1; print; }
+
+            # Stop on next entry after printing:
+            inbody && $0 ~ /^ *\[/ { exit 0; }
+            ' "${eefile}")"
+
+            test -n "${ee_name}" && break
+        done <<EOF
+$(find "${eepath}" -type f -name 'ee.txt')
+EOF
+        test -n "${ee_name}" && break
+    done <<EOF
+${EEPATH}
+EOF
+    # Execute if the environment was found:
+    if test -n "${ee_name}" ; then
+        if test -n "${selectonly}" ; then
+            echo "Selected '${ee_desc:-${env_name}}': ${ee_user}@${ee_host}"
+        else
+            if [ "${ee_cmd}" != "" ] ; then
+                eex ${ee_cmd}
+            else
+                eex "$@"
+            fi
+        fi
+        return 0
+    fi
+
+    echo "No environment found." 1>&2
+    return 1
+}
+
+# ##############################################################################
 # Virtualbox
 
 # Function mountvboxsf - Mount virtualbox shared folder.
