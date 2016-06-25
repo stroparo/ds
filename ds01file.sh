22ff355285a5aae62649bdb2cb6cca475132c18f
Cristian Stroparo <cstropz@gmail.com>
2016-06-24 22:28:11 -0300

    Removed 01adminpc: moved virtualbox shared mounting func to ds01file, and installers to new 01installers.

--- ds01file.sh
+++ ds01file.sh
@@ -562,4 +562,23 @@ dugt10desc () {
 }
 
 # ##############################################################################
+# Virtualbox
 
+# Function mountvboxsf - Mount virtualbox shared folder.
+# Syntax: path-to-dir (sharing will be named as its basename)
+unset mountvboxsf
+mountvboxsf () {
+
+    [ -n "${1}" ] || return 1
+    [ -d "${1}" ] || sudo mkdir "${1}"
+
+    sudo mount -t vboxsf -o rw,uid="${USER}",gid="$(id -gn)" "$(basename ${1})" "${1}"
+
+    if [ "$?" -eq 0 ] ; then
+        cd "${1}"
+        pwd
+        ls -FlA
+    fi
+}
+
+# ##############################################################################
