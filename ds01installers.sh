677c23466997706f5fe1f7e0aa8f42339069a206
Cristian Stroparo <cstropz@gmail.com>
2016-06-28 21:54:37 -0300

    01installers online install* functions standardized messages and verifications.

--- ds01installers.sh
+++ ds01installers.sh
@@ -9,10 +9,14 @@
 # Function installohmyzsh - Install Oh My ZSH.
 unset installohmyzsh
 installohmyzsh () {
+
     echo '==> Installing ohmyzsh..' 1>&2
 
     if which zsh >/dev/null && [ ! -d "${HOME}/.oh-my-zsh" ] ; then
         sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+    else
+        echo 'FATAL: Must have zsh installed.'
+        return 1
     fi
 }
 
@@ -20,24 +24,41 @@ installohmyzsh () {
 # Syntax: {package-filename}
 unset installtruecrypt
 installtruecrypt () {
+
     typeset pkg="${1}"
     typeset pkgdir="$(dirname "${pkg}")"
     typeset pkginstaller="${pkgdir}/truecrypt-7.1a-setup-x64"
     typeset truecryptpath="/usr/bin/truecrypt"
+
     echo '==> Installing truecrypt..' 1>&2
 
-    if _is_linux ; then
-        if [ -e "${truecryptpath}" ] ; then
-            echo "Truecrypt already installed at '${truecryptpath}'" 1>&2
-            return 0
-        elif [ ! -e "${pkg}" ] ; then
-            echo "Missing required package '${pkg}'." 1>&2
-            return 1
-        else
-            tar -xzf "${pkg}" -C "${pkgdir}"
-            echo "Installing '${pkginstaller}'.." 1>&2
-            "${pkginstaller}" && rm -f "${pkginstaller}"
+    if ! _is_linux ; then
+
+        echo 'SKIP: Not in Linux, so nothing done.' 1>&2
+        return
+
+    elif [ -e "${truecryptpath}" ] ; then
+
+        echo "SKIP: Truecrypt already installed at '${truecryptpath}'" 1>&2
+        return
+
+    elif [ ! -e "${pkg}" ] ; then
+
+        echo "FATAL: Missing required package '${pkg}'." 1>&2
+        return 1
+
+    elif tar -xzf "${pkg}" -C "${pkgdir}" ; then
+
+        echo "Installing '${pkginstaller}'.." 1>&2
+
+        if "${pkginstaller}" ; then
+            rm -f "${pkginstaller}"
         fi
+
+        echo 'Truecrypt installation complete.' 1>&2
+    else
+        echo 'FATAL: Truecrypt installation failed.' 1>&2
+        return 1
     fi
 }
 
@@ -47,18 +68,27 @@ installtruecrypt () {
 unset installdropbox
 installdropbox () {
 
-    typeset pname=installdropbox
-
     echo '==> Installing dropbox..' 1>&2
 
-    if _is_linux ; then
-        if [ ! -e ~/.dropbox-dist/dropboxd ] ; then
-            cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+    if ! _is_linux ; then
 
-            # Desktop tray workaround (export empty DBUS_SESSION_BUS_ADDRESS for process):
-            # This first approach doesnt work as Dropbox often updates itself:
-            # sed -i -e 's/^exec.*dropbox/export DBUS_SESSION_BUS_ADDRESS=""; &/' ~/.dropbox-dist/dropboxd
-            cat > ~/.config/autostart/dropbox.desktop <<EOF
+        echo 'SKIP: Not in Linux, so nothing done.' 1>&2
+        return
+
+    elif [ -e ~/.dropbox-dist/dropboxd ] ; then
+
+        echo 'SKIP: It was installed already.' 1>&2
+        return
+
+    fi
+
+    cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | \
+        tar xzf -
+
+    # Desktop tray workaround (export empty DBUS_SESSION_BUS_ADDRESS for process):
+    # This first approach doesnt work as Dropbox often updates itself:
+    # sed -i -e 's/^exec.*dropbox/export DBUS_SESSION_BUS_ADDRESS=""; &/' ~/.dropbox-dist/dropboxd
+    cat > ~/.config/autostart/dropbox.desktop <<EOF
 [Desktop Entry]
 Encoding=UTF-8
 Version=0.9.4
@@ -71,16 +101,10 @@ StartupNotify=false
 Terminal=false
 Hidden=false
 EOF
-            elog -n "$pname" 'Launching dropbox..'
-            env DBUS_SESSION_BUS_ADDRESS='' "${HOME}"/.dropbox-dist/dropboxd > /dev/null 2>&1 &
+    echo 'Launching dropbox..' 1>&2
+    env DBUS_SESSION_BUS_ADDRESS='' "${HOME}"/.dropbox-dist/dropboxd > /dev/null 2>&1 &
 
-            elog -n "$pname" 'Completed.'
-        else
-            elog -s -n "$pname" 'It was installed already.'
-        fi
-    else
-        elog -s -n "$pname" 'Not in Linux, so nothing done.'
-    fi
+    echo 'Dropbox installation complete.' 1>&2
 }
 
 # Function installinputfont - Installs local input font package
@@ -88,7 +112,6 @@ EOF
 unset installinputfont
 installinputfont () {
 
-    typeset pname=installinputfont
     typeset find_command="find \"$HOME/Input_Fonts\" \( -name '*.[o,t]tf' -or -name '*.pcf.gz' \) -type f -print0"
     typeset font_dir="$HOME/.local/share/fonts"
     typeset inputfontpackage="$1"
@@ -96,28 +119,44 @@ installinputfont () {
     echo '==> Installing input font..' 1>&2
 
     if ! _is_linux ; then
-        elog -s -n "$pname" 'Only Linux supported.'
+
+        echo 'SKIP: Not in Linux, so nothing done.' 1>&2
         return
-    elif [ ! -e "$inputfontpackage" ] ; then
-        elog -f -n "$pname" 'Invalid input font package location (first argument).'
-        return 1
+
     elif [ -e "$font_dir/InputMono-Regular.ttf" ] ; then
-        elog -s -n "$pname" 'It was installed already.'
+
+        echo 'SKIP: It was installed already.' 1>&2
         return
+
+    elif [ ! -e "$inputfontpackage" ] ; then
+
+        echo 'FATAL: Invalid input font package location (first argument).' 1>&2
+        return 1
+
     fi
 
     unzip -d "$HOME" "$inputfontpackage" 'Input_Fonts/*'
-    test -d "$font_dir" || mkdir -p "$font_dir"
+
+    if [ ! -d "$font_dir" ] ; then
+        mkdir -p "$font_dir"
+    fi
+
     eval $find_command | xargs -0 -I % cp "%" "$font_dir/"
     rm -rf "$HOME/Input_Fonts"
 
-    # Reset font cache on Linux
-    if command -v fc-cache @>/dev/null ; then
-        elog -n "$pname" "Resetting font cache, this may take a moment..."
-        fc-cache -f "$font_dir"
-    fi
+    if [ -e "$font_dir/InputMono-Regular.ttf" ] ; then
 
-    elog -n "$pname" "Finished installing fonts to '$font_dir'."
+        # Reset font cache on Linux
+        if command -v fc-cache @>/dev/null ; then
+            echo "Resetting font cache, this may take a moment..." 1>&2
+            fc-cache -f "$font_dir"
+        fi
+
+        echo "Input fonts installed into '$font_dir'." 1>&2
+    else
+        echo "FATAL: Failure installing input fonts to '$font_dir'." 1>&2
+        return 1
+    fi
 }
 
 # Function installpowerfonts - Install powerline fonts.
@@ -126,16 +165,27 @@ installpowerfonts () {
 
     echo '==> Installing powerline fonts..' 1>&2
 
-    if _is_linux ; then
-        if [ ! -e "$HOME/.local/share/fonts/Inconsolata for Powerline.otf" ] ; then
-            wget https://github.com/powerline/fonts/archive/master.zip -O ~/powerline.zip
-            (cd ~ ; unzip powerline.zip)
-            ~/fonts-master/install.sh && rm -rf ~/fonts-master ~/powerline.zip
-        else
-            echo 'It was installed already.' 1>&2
-        fi
+    if ! _is_linux ; then
+
+        echo 'SKIP: Not in Linux, so nothing done.' 1>&2
+        return
+
+    elif [ -e "$HOME/.local/share/fonts/Inconsolata for Powerline.otf" ] ; then
+
+        echo 'SKIP: It was installed already.' 1>&2
+        return
+    fi
+
+    wget https://github.com/powerline/fonts/archive/master.zip -O ~/powerline.zip
+    (cd ~ ; unzip powerline.zip)
+
+    if ~/fonts-master/install.sh ; then
+
+        rm -rf ~/fonts-master ~/powerline.zip
+        echo 'Powerfonts sucessfully installed.' 1>&2
     else
-        echo 'Not in Linux, so nothing done.' 1>&2
+        echo 'FATAL: Failure during powerfonts installation.' 1>&2
+        return 1
     fi
 }
 
@@ -147,15 +197,24 @@ installyoutubedl () {
 
     echo '==> Installing youtube-dl..' 1>&2
 
-    if _is_linux ; then
-        if [ ! -e "${youtubedlpath}" ] ; then
-            sudo wget 'https://yt-dl.org/latest/youtube-dl' -O "${youtubedlpath}"
-            sudo chmod a+rx "${youtubedlpath}"
-        else
-            echo 'It was installed already.' 1>&2
-        fi
+    if ! _is_linux ; then
+
+        echo 'SKIP: Not in Linux, so nothing done.' 1>&2
+        return
+
+    elif [ -e "${youtubedlpath}" ] ; then
+
+        echo 'SKIP: It was installed already.' 1>&2
+        return
+    fi
+
+    sudo wget -q -O "${youtubedlpath}" 'https://yt-dl.org/latest/youtube-dl'
+    sudo chmod a+rx "${youtubedlpath}"
+
+    if ls -l "${youtubedlpath}" ; then
+        echo 'Installation complete.' 1>&2
     else
-        echo 'Not in Linux, so nothing done.' 1>&2
+        echo 'FATAL: Installation failed.' 1>&2
     fi
 }
 
