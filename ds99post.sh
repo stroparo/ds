94e8bbc7cc9188f434d0915f9fe3b38bf8b9f306
Cristian Stroparo <cstropz@gmail.com>
2016-07-03 17:08:44 -0300

    Added pathmunge to 99post.

--- ds99post.sh
+++ ds99post.sh
@@ -1,19 +1,24 @@
 # DS - Daily Shells Library
 # License:
-#  See README.md document in projects page at
-#  https://github.com/stroparo/ds
+#   See README.md document in projects page at
+#   https://github.com/stroparo/ds
 
 # ##############################################################################
 # Post file
 
-# Params:
+# ##############################################################################
+# Globals
+
 DS_GLOB="ds.sh ds[0-9]*sh aliases.sh sshagent.sh"
 DS_LOADED=true
 
-# Aliases:
+# ##############################################################################
+# Main
+
 unalias d 2>/dev/null
 
-# Calls:
 chmodshells "${DS_HOME}/bin" "${DS_HOME}/scripts"
+pathmunge "${DS_HOME}/bin" "${DS_HOME}/scripts"
 aliasnoext "${DS_HOME}/bin" "${DS_HOME}/scripts"
+
 runcommands "${DS_POST_CALLS}"
