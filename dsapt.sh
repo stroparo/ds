c03cfb1dab68820ac1d1a5225b7d4f19a2c5cbf9
Cristian Stroparo <cstropz@gmail.com>
2016-11-25 13:49:19 -0200

    Fmt.

--- dsapt.sh
+++ dsapt.sh
@@ -9,6 +9,63 @@
 ckapt () { which apt > /dev/null || which apt-get > /dev/null ; }
 ckaptitude () { aptitude -h > /dev/null || (sudo apt-get update && sudo apt-get install -y aptitude) ; }
 
+aptaddppa () {
+    # Adds ubuntu ppa repositories listed in the filename argument.
+    # Syn: {ppa-list-filename}
+
+    typeset somefail=false
+
+    typeset ppalistfile="$1"
+    typeset usage="${pname} {ppa file (one ppa path per line)}"
+
+    _is_ubuntu || return 1
+    test -f "$ppalistfile" || return 1
+
+    [[ -n $ZSH_VERSION ]] && set -o shwordsplit
+
+    for ppa in "$(cat "$ppalistfile")" ; do
+
+        if ! (ls -1 /etc/apt/sources.list.d | grep -q "$(echo "$ppa" | sed -e 's#/#-#g')")
+        then
+            elog "ppa '${ppa}' ..."
+
+            if ! sudo apt-add-repository "ppa:${ppa}" ; then
+                elog -f "ppa '${ppa}'."
+                somefail=true
+            fi
+        else
+            elog -s "ppa '${ppa}' already present."
+        fi
+
+    done
+
+    [[ -n $ZSH_VERSION ]] && set +o shwordsplit
+
+    if $somefail ; then
+        elog -f 'Some ppa failed.'
+        return 1
+    fi
+}
+
+dpkgstat () {
+    # Displays installation status of given package names
+    # (dpkg -s "$@" | awk ... /^Package:/ ... /^Status:/).
+    # Syn: {pkg1} {pkg2} ... {pkgN}
+
+    [ "${#}" -lt 1 ] && return 1
+
+    dpkg -s "$@" | \
+        awk '
+            /^Package:/ { pkg = $0; }
+            /^Status:/ {
+                stat = $0; printf("%-32s%s\n", pkg, stat);
+            }
+        '
+}
+
+# ##############################################################################
+# aptdeploy and helpers
+
 aptclean () {
     # Cleans up ubuntu packages and unwanted files.
     # Rmk - this also installs localepurge, but it must be executed separately (in that
@@ -80,6 +137,17 @@ aptinstall () {
     fi
 }
 
+fixaptmodes () {
+    # Fix workaround for /etc/apt/sources.list.d mode issue.
+    #   This will sudo chmod 644 to all files in /etc/apt/sources.list.d
+    # Rmk:
+    #   Common scenario this glitch happens is a call to update after adding a ppa repo.
+
+    if [ -d /etc/apt/sources.list.d ] ; then
+        sudo chmod 644 /etc/apt/sources.list.d/*
+    fi
+}
+
 aptdeploy () {
     # Installs apt packages.
     # Calls aptinstall arguments,
@@ -119,68 +187,5 @@ aptdeploy () {
     fi
 }
 
-dpkgstat () {
-    # Displays installation status of given package names
-    # (dpkg -s "$@" | awk ... /^Package:/ ... /^Status:/).
-    # Syn: {pkg1} {pkg2} ... {pkgN}
-
-    [ "${#}" -lt 1 ] && return 1
-
-    dpkg -s "$@" | \
-        awk '
-            /^Package:/ { pkg = $0; }
-            /^Status:/ {
-                stat = $0; printf("%-32s%s\n", pkg, stat);
-            }
-        '
-}
-
-fixaptmodes () {
-    # Fix workaround for /etc/apt/sources.list.d mode issue.
-    #   This will sudo chmod 644 to all files in /etc/apt/sources.list.d
-    # Rmk:
-    #   Common scenario this glitch happens is a call to update after adding a ppa repo.
-
-    if [ -d /etc/apt/sources.list.d ] ; then
-        sudo chmod 644 /etc/apt/sources.list.d/*
-    fi
-}
-
-aptaddppa () {
-    # Adds ubuntu ppa repositories listed in the filename argument.
-    # Syn: {ppa-list-filename}
-
-    typeset somefail=false
-
-    typeset ppalistfile="$1"
-    typeset usage="${pname} {ppa file (one ppa path per line)}"
-
-    _is_ubuntu || return 1
-    test -f "$ppalistfile" || return 1
-
-    [[ -n $ZSH_VERSION ]] && set -o shwordsplit
-
-    for ppa in "$(cat "$ppalistfile")" ; do
-
-        if ! (ls -1 /etc/apt/sources.list.d | grep -q "$(echo "$ppa" | sed -e 's#/#-#g')")
-        then
-            elog "ppa '${ppa}' ..."
-
-            if ! sudo apt-add-repository "ppa:${ppa}" ; then
-                elog -f "ppa '${ppa}'."
-                somefail=true
-            fi
-        else
-            elog -s "ppa '${ppa}' already present."
-        fi
-
-    done
-
-    [[ -n $ZSH_VERSION ]] && set +o shwordsplit
-
-    if $somefail ; then
-        elog -f 'Some ppa failed.'
-        return 1
-    fi
-}
+# ##############################################################################
 
