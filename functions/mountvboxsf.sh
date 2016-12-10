ed021db78d7ec9f5bd6402127a2d4a0b10e16c02
Cristian Stroparo <cstropz@gmail.com>
2016-12-10 00:53:50 -0200

    Sent mountvboxsf to new function file in functions dir, from old dsvirtualbox.sh

---
+++ functions/mountvboxsf.sh
@@ -0,0 +1,19 @@
+# DS - Daily Shells Library
+# License:
+#  See README.md document in projects page at
+#  https://github.com/stroparo/ds
+
+mountvboxsf () {
+    # Info: Mount virtualbox shared folder.
+    # Syntax: path-to-dir (sharing will be named as its basename)
+
+    [ -n "${1}" ] || return 1
+    [ -d "${1}" ] || sudo mkdir "${1}"
+
+    if sudo mount -t vboxsf -o rw,uid="${USER}",gid="$(id -gn)" "$(basename ${1})" "${1}"
+    then
+        cd "${1}"
+        pwd
+        ls -AFl
+    fi
+}
