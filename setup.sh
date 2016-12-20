394f1fd812a77bc0ca464f14175da19f5b5ed55f
Cristian Stroparo <cstropz@gmail.com>
2016-12-19 22:40:45 -0200

    setup.sh now works with wget ... -O - | bash

--- setup.sh
+++ setup.sh
@@ -1,53 +1,20 @@
 #!/usr/bin/env sh
 
-export DS_HOME="${1:-${HOME}/.ds}"
-export PROFILE_PATH="${2:-${HOME}/.bashrc}"
+DS_LOAD_CODE='[ -r "${HOME}/.ds/ds.sh" ] && source "${HOME}/.ds/ds.sh" "${HOME}/.ds" 1>&2'
 
-export SOURCE_DS_CODE=". '${DS_HOME}/ds.sh' '${DS_HOME}'"
-
-checkEnv () {
-
-    if [ ! -r "${DS_HOME}/ds.sh" ] ; then
-
-        echo "FATAL: DS_HOME='${DS_HOME}' must be the DS directory." 1>&2
-        exit 1
-
-    elif [ -e "${PROFILE_PATH}" ] ; then
-
-        if [ ! -r "${PROFILE_PATH}" -o ! -w "${PROFILE_PATH}" ] ; then
-            echo "FATAL: '${PROFILE_PATH}' exists but no read or write mode enabled." 1>&2
-            exit 1
-        fi
-
-        if grep 'ds.sh' "${PROFILE_PATH}" /dev/null ; then
-            echo "SKIP: DS already exists in the profile. Nothing done." 1>&2
-            exit
-        fi
-
-    fi
+dsget () {
+    rm -rf "$HOME/.ds" > /dev/null 1>&2
+    wget 'https://github.com/stroparo/ds/archive/master.zip' -O "$HOME/.ds.zip" && \
+        unzip "$HOME/.ds.zip" -d "$HOME" && \
+        mv "$HOME/ds-master" "$HOME/.ds"
 }
 
-main () {
+dsget || exit $?
 
-    if . "${DS_HOME}/ds.sh" "${DS_HOME}" && ${DS_LOADED:-false}; then
+if . "$HOME/.ds.sh" "$HOME/.ds" 1>&2 && [ -z "${DS_LOADED}" ] ; then
+    [ -e "$HOME/.bashrc" ]  && appendunique "DS_LOAD_CODE" "$HOME/.bashrc"
+    [ -e "$HOME/.zshrc" ]   && appendunique "DS_LOAD_CODE" "$HOME/.zshrc"
+else
+    exit 99
+fi
 
-        # Sourced DS, so now append it to the profile:
-
-        if egrep -q '([.]|source) .*/ds.sh' "${PROFILE_PATH}" 2>/dev/null && \
-            egrep -q '^[^#]*/ds.sh' "${PROFILE_PATH}" 2>/dev/null
-        then
-            echo "SKIP: DS already exists in the profile. Nothing done." 1>&2
-            exit
-        else
-            appendunique "${SOURCE_DS_CODE}" "${PROFILE_PATH}"
-        fi
-
-    else
-        echo "DS_HOME=${DS_HOME}" 1>&2
-        echo "PROFILE_PATH=${PROFILE_PATH}" 1>&2
-        echo "FATAL: failed setting DS up because it could not be loaded." 1>&2
-    fi
-}
-
-checkEnv
-main
