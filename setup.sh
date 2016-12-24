00ddb548641275373e63fe42d7d9aae1fbdcc09e
Cristian Stroparo <cstropz@gmail.com>
2016-12-24 00:30:19 -0200

    appendunique call to .*shrc now properly independs of them already existing.

--- setup.sh
+++ setup.sh
@@ -14,8 +14,8 @@ dsget || exit $?
 . "$HOME/.ds/ds.sh" "$HOME/.ds" >/dev/null 2>&1
 
 if [ -n "${DS_LOADED}" ] ; then
-    [ -e "$HOME/.bashrc" ]  && appendunique "$DS_LOAD_CODE" "$HOME/.bashrc"
-    [ -e "$HOME/.zshrc" ]   && appendunique "$DS_LOAD_CODE" "$HOME/.zshrc"
+    appendunique "$DS_LOAD_CODE" "$HOME/.bashrc"
+    appendunique "$DS_LOAD_CODE" "$HOME/.zshrc"
 else
     echo 'FATAL: ds not loaded so source commands were not written to any shell profiles..' 1>&2
     exit 99
