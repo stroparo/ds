39dddd4ffd9f1507cb3cdd37df2794ccc57a277e
Cristian Stroparo <cstropz@gmail.com>
2017-01-05 19:48:29 -0200

    gitconfig() renamed to dsgitconfig() and added new dsgitdeploy (uses dsgitconfig).

--- functions/opsgit.sh
+++ functions/opsgit.sh
@@ -30,12 +30,11 @@ ${1}
 EOF
 }
 
-gitconfig () {
+dsgitconfig () {
     # Info: Configure git.
     # Syntax: {email} {name} [other git config --global options]
-    # Example: gitconfig "john@doe.com" "John Doe" 'core.autocrlf false' 'push.default simple'
+    # Example: dsgitconfig "john@doe.com" "John Doe" 'core.autocrlf false' 'push.default simple'
 
-    typeset pname=gitconfig
     typeset email gitfile name
 
     typeset oldind="${OPTIND}"
@@ -50,7 +49,7 @@ gitconfig () {
     shift $((OPTIND-1)) ; OPTIND="${oldind}"
 
     if [ -n "$gitfile" ] && _any_not_w "${gitfile}"; then
-        elog -f -n "${pname}" "Must pass writeable file to -f option."
+        echo "FATAL: Must pass writeable file to -f option." 1>&2
         return 1
     fi
 
@@ -62,6 +61,33 @@ gitconfig () {
         [ -n "${email}" ] && git config --global user.email "${email}"
         [ -n "${name}" ] &&  git config --global user.name "${name}"
         for i in "$@" ; do git config --global $(echo ${i}) ; done
+    fi
+}
+
+dsgitdeploy () {
+    # Info: Configures git. Also handles windows installation if in cygwin.
+    # Syn: List of quoted params for dsgitconfig(), eg:
+    #   "'core.autocrlf false' 'push.default simple'"
+
+    which git >/dev/null 2>&1 || aptinstall -y 'git-core'
+    which git >/dev/null 2>&1 || return 1
+
+    readmyemail
+    readmysign
+    [ -e ~/.ssh/id_rsa ] || sshkeygenrsa "${MYEMAIL}"
+
+    eval dsgitconfig -e "\"${MYEMAIL}\"" -n "\"${MYSIGN}\"" \
+        "$(echo "$1")"
+
+    if _is_cygwin ; then
+        typeset cyggitconfig="$(cygpath "$USERPROFILE")/.gitconfig"
+        touch "$cyggitconfig"
+
+        dsgitconfig 'core.filemode false'
+        dsgitconfig -f "${cyggitconfig}" 'core.filemode false'
+
+        eval dsgitconfig -f "\"${cyggitconfig}\"" -e "\"${MYEMAIL}\"" -n "\"${MYSIGN}\"" \
+            "$(echo "$1")"
     fi
 }
 
