bfb408c85beebd2db2dc546dbab95ea1e7fb7132
Cristian Stroparo <cstropz@gmail.com>
2017-01-05 19:26:34 -0200

    Refactor.

--- functions/opsapt.sh
+++ functions/opsapt.sh
@@ -49,12 +49,54 @@ EOF
 # ##############################################################################
 # aptdeploy and helpers
 
+aptinstall () {
+    # Installs packages listed in file.
+    # Syn: [-u] [-y] filename
+    # -u means do upgrade
+    # -y means do assume yes (as per vanilla apt)
+
+    typeset assumeyes
+    typeset doupdate=true
+    typeset doupgrade=false
+
+    typeset oldind="${OPTIND}"
+    OPTIND=1
+    while getopts ':fuy' option ; do
+        case "${option}" in
+        f) doupdate=false;;
+        u) doupgrade=true;;
+        y) assumeyes='-y';;
+        esac
+    done
+    shift $((OPTIND-1)) ; OPTIND="${oldind}"
+
+    ckaptitude || return 1
+    if ${doupdate} ; then sudo aptitude update || return 1 ; fi
+    if ${doupgrade} ; then sudo aptitude upgrade ${assumeyes} || return 1 ; fi
+
+    if [ -f "$1" ] && [ -s "$1" ] ; then
+        sudo aptitude install ${assumeyes} -Z $(sed -e 's/#.*$//' "$@" | grep .)
+    elif [[ $1 != dummy ]] ; then
+        sudo aptitude install ${assumeyes} -Z "$@"
+    fi
+}
+
+fixaptmodes () {
+    # Fix workaround for /etc/apt/sources.list.d mode issue.
+    #   This will sudo chmod 644 to all files in /etc/apt/sources.list.d
+    # Rmk:
+    #   Common scenario this glitch happens is a call to update after adding a ppa repo.
+
+    if [ -d /etc/apt/sources.list.d ] ; then
+        sudo chmod 644 /etc/apt/sources.list.d/*
+    fi
+}
+
 aptclean () {
     # Cleans up ubuntu packages and unwanted files.
     # Rmk - this also installs localepurge, but it must be executed separately (in that
     #   package you will choose only the locales you use and/or want to keep).
 
-    typeset rmorphan
     ckapt || return "$?"
     which deborphan > /dev/null 2>&1 || sudo aptitude install -y deborphan
     which localepurge > /dev/null 2>&1 || sudo aptitude install -y localepurge
@@ -76,61 +118,6 @@ aptclean () {
     elog '==> Complete.'
 }
 
-aptinstall () {
-    # Installs packages listed in file.
-    # Syn: [-u] [-y] filename
-    # -u means do upgrade
-    # -y means do assume yes (as per vanilla apt)
-
-    typeset assumeyes
-    typeset doupdate=true
-    typeset doupgrade=false
-
-    typeset oldind="${OPTIND}"
-    OPTIND=1
-    while getopts ':nuy' option ; do
-        case "${option}" in
-        n) doupdate=false;;
-        u) doupgrade=true;;
-        y) assumeyes='-y';;
-        esac
-    done
-    shift $((OPTIND-1)) ; OPTIND="${oldind}"
-
-    # Prep:
-
-    if [[ $1 != dummy ]] && [ ! -r "${1}" ] ; then
-        echo "A readable packagelist file must be passed as the first argument. Aborted." 1>&2
-        return 1
-    fi
-    ckaptitude || return 1
-
-    if ${doupdate} ; then
-        sudo aptitude update || return 2
-    fi
-
-    if ${doupgrade} ; then
-        sudo aptitude upgrade ${assumeyes} || return 11
-    fi
-
-    if [ -f "$1" ] ; then
-        sudo aptitude install ${assumeyes} -Z $(sed -e 's/#.*$//' "$@" | grep .) || return 21
-    elif [[ $1 != dummy ]] ; then
-        sudo aptitude install ${assumeyes} -Z "$@" || return 22
-    fi
-}
-
-fixaptmodes () {
-    # Fix workaround for /etc/apt/sources.list.d mode issue.
-    #   This will sudo chmod 644 to all files in /etc/apt/sources.list.d
-    # Rmk:
-    #   Common scenario this glitch happens is a call to update after adding a ppa repo.
-
-    if [ -d /etc/apt/sources.list.d ] ; then
-        sudo chmod 644 /etc/apt/sources.list.d/*
-    fi
-}
-
 aptdeploy () {
     # Installs apt packages using the routines: aptinstall, fixaptmodes, aptclean.
     # Deps: apt or apt-get.
@@ -139,25 +126,12 @@ aptdeploy () {
     typeset ask=false
     typeset upgradeoption
 
-    typeset oldind="${OPTIND}"
-    OPTIND=1
-    while getopts ':i' option ; do
-        case "${option}" in
-        i) ask=true;;
-        esac
-    done
-    shift $((OPTIND-1)) ; OPTIND="${oldind}"
+    [[ $1 = -i ]] && ask=true && shift
 
     ckapt || return "$?"
     _any_not_r "$@" && return 1
-
-    if [[ $- = *i* ]] && $ask && ! userconfirm 'Proceed deploying APT assets?' ; then
-        return
-    fi
-
-    if [[ $- = *i* ]] && userconfirm 'Upgrade all packages?' ; then
-        upgradeoption='u'
-    fi
+    [[ $- = *i* ]] && $ask && ! userconfirm 'Proceed deploying APT assets?' && return
+    [[ $- = *i* ]] && userconfirm 'Upgrade all packages?' && upgradeoption='u'
 
     aptinstall -${upgradeoption}y "$@"
     fixaptmodes
