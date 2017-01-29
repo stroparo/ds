b4cf2b0d6a8727617bee691d3794155c00060433
Cristian Stroparo <cstropz@gmail.com>
2017-01-29 15:21:03 -0200

    Refactor.

--- functions/aliasops.sh
+++ functions/aliasops.sh
@@ -3,26 +3,14 @@
 #  See README.md document in projects page at
 #  https://github.com/stroparo/ds
 
-alias edithosts='sudo vi /etc/hosts'
-alias editsshauth='mkdir ~/.ssh 2>/dev/null ; vi ~/.ssh/authorized_keys'
-
-alias psfe='ps -fe'
-alias psfens='ps -fe | grep -v bash | grep -v sshd'
+alias psef='ps -ef'
+alias psefnoshells='ps -ef | grep -v bash | grep -v sshd'
+alias psefuser='ps -ef|grep "${USER}"'
 alias psfu='ps -fu "${UID:-$(id -u)}" -U "${UID:-$(id -u)}"'
-alias psfuns='ps -fu "${UID:-$(id -u)}" -U "${UID:-$(id -u)}" | grep -v bash | grep -v sshd'
-alias psu='ps -ef|grep "${USER}"'
-alias psuu='ps -u "${UID:-$(id -u)}" -U "${UID:-$(id -u)}" u'
-alias psuunosh='ps -u "${UID:-$(id -u)}" -U "${UID:-$(id -u)}" u | grep -v bash | grep -v sshd'
+alias psfunoshells='ps -fu "${UID:-$(id -u)}" -U "${UID:-$(id -u)}" | grep -v bash | grep -v sshd'
 
-# Aliases - admin - IBM AIX platform:
+# IBM AIX platform:
 if [[ $(uname) = *[Aa][Ii][Xx]* ]] ; then
-
     alias psft='ps -fT1'
     alias psftu='ps -fT1|awk "\$1 ~ /^$USER$/"'
-
-# Aliases - admin - Linux & Cygwin:
-# elif [[ "$(uname -a)" = *[Ll]inux* ]] || [[ "$(uname -a)" = *[Cc]ygwin* ]] ; then
-
-#     ...
-
 fi
