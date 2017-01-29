69d3617565bb9b65de80cb87eb4346e4c712a006
Cristian Stroparo <cstropz@gmail.com>
2017-01-29 15:35:37 -0200

    Formatting.

--- functions/ops.sh
+++ functions/ops.sh
@@ -10,12 +10,10 @@ edithosts () { sudo vi /etc/hosts ; }
 editsshauth () { mkdir ~/.ssh 2>/dev/null ; vi ~/.ssh/authorized_keys ; }
 mountiso () { sudo mount -o loop -t iso9660 "$@" ; }
 pgr () { ps -ef | egrep -i "$1" | egrep -v "grep.*(${1})" ; }
-# psef () { ps -ef ; }
-# psefnoshells () { ps -ef | grep -v bash | grep -v sshd ; }
-# psefuser () { ps -ef | grep "${USER}" ; }
-# psfu () { ps -fu "${UID:-$(id -u)}" -U "${UID:-$(id -u)}" ; }
-# psfunoshells () { ps -fu "${UID:-$(id -u)}" -U "${UID:-$(id -u)}" | grep -v bash | grep -v sshd ; }
-
+psef () { ps -ef ; }
+psefnoshells () { ps -ef | grep -v bash | grep -v zsh | grep -v sshd ; }
+psefuser () { ps -ef | grep "${USER}" ; }
+psfu () { ps -fu "${UID:-$(id -u)}" -U "${UID:-$(id -u)}" ; }
 autobash () {
     appendunique 'if [[ $- = *i* ]] && [ -z "${BASH_VERSION}" ] ; then bash ; fi' \
         "$HOME/.profile"
@@ -33,6 +31,13 @@ pgralert () {
 
     while pgr "${1}" > /dev/null ; do sleep 1 ; done
     while true ; do echo '\a' ; sleep 8 ; done
+}
+
+psfunoshells () {
+    ps -fu "${UID:-$(id -u)}" -U "${UID:-$(id -u)}" \
+    | grep -v bash \
+    | grep -v zsh \
+    | grep -v sshd
 }
 
 setlogdir () {
@@ -67,7 +72,14 @@ shut () {
 }
 
 # ##############################################################################
-# IBM AIX platform:
+# Control script wrappers
+
+cta () { sudo "/etc/init.d/apache${2:-2}"   "${1:-restart}" ; }
+ctlamp () { "${LAMPHOME}/ctlscript.sh"      "${1:-restart}" ; }
+ctpg () { sudo "/etc/init.d/postgresql${2}" "${1:-restart}" ; }
+
+# ##############################################################################
+# IBM AIX platform
 
 if [[ $(uname) = *[Aa][Ii][Xx]* ]] ; then
     psft () { ps -fT1 ; }
