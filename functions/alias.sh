e5d1e386d03f4dc3162472bc57586639553ad4b2
Cristian Stroparo <cstropz@gmail.com>
2017-02-07 16:33:48 -0200

    Formatting.

--- functions/alias.sh
+++ functions/alias.sh
@@ -4,17 +4,43 @@
 #  https://github.com/stroparo/ds
 
 # ##############################################################################
-# Aliases aliases
-alias xcd="alias | egrep \"'c?d \" | fgrep -v 'cd -'"
-alias xgit="alias | grep 'git '"
+# Aliases
 
+{
 alias cls='clear'
 alias dfg='df -gP'
 alias dfh='df -hP'
 alias dums='du -ma | sort -n'
 alias dumg='du -ma | sort -rn'
+if which exa >/dev/null 2>&1 ; then alias e='exa -il'; alias ea='exa -ila'; fi
 alias findd='find . -type d'
 alias findf='find . -type f'
+alias xcd="alias | egrep \"'c?d \" | fgrep -v 'cd -'"
+alias xgit="alias | grep 'git '"
+
+if [[ $(grep --version 2>/dev/null) = *GNU* ]] ; then
+    alias grep='grep --color=auto'
+    alias egrep='egrep --color=auto'
+    alias fgrep='fgrep --color=auto'
+fi
+
+if [[ $(ls --version 2>/dev/null) = *GNU* ]] ; then
+    alias ls='ls --color=auto'
+    alias l='ls -Flhi'
+    alias ll='ls -AFlhi'
+    alias lt='ls -Flrthi'
+else
+    alias l='ls -Fl'
+    alias ll='ls -AFl'
+    alias lt='ls -Flrt'
+fi
+}
+
+# ##############################################################################
+# Etc
+
+alias nhr='rm nohup.out'
+alias nht='tail -9999f nohup.out'
 alias sb='subl'
 alias tpf='typeset -f'
 alias tps='typeset'
@@ -22,38 +48,6 @@ alias ya='youtube-dl -f 140'
 alias yabest='youtube-dl -f bestaudio'
 alias yd='youtube-dl'
 alias yx='youtube-dl -x'
-
-# ##############################################################################
-# Grep color
-if [[ $(grep --version 2>/dev/null) = *GNU* ]] ; then
-    alias grep='grep --color=auto'
-    alias egrep='egrep --color=auto'
-    alias fgrep='fgrep --color=auto'
-fi
-
-# ##############################################################################
-# No hang up (nohup)
-alias nhr='rm nohup.out'
-alias nht='tail -9999f nohup.out'
-
-# ##############################################################################
-# Ls etcetera
-
-if [[ $(ls --version 2>/dev/null) = *GNU* ]] ; then
-    alias ls='ls --color=auto'
-    alias l='ls -Fhil'
-    alias ll='ls -FhilA'
-    alias lt='ls -Fhilrt'
-else
-    alias l='ls -Fl'
-    alias ll='ls -AFl'
-    alias lt='ls -Flrt'
-fi
-
-if which exa >/dev/null 2>&1 ; then
-    alias e='exa -il'
-    alias ea='exa -ila'
-fi
 
 # ##############################################################################
 
