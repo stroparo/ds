5269ccb428330d8b6fae1bd5b5fc2f0cd3ab0172
Cristian Stroparo <cstropz@gmail.com>
2017-03-28 14:24:12 -0300

    Refactor.

--- setup.sh
+++ setup.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env sh
 
+INSTALL_DIR=${1:-$HOME/.ds}
 DS_LOAD_CODE='[ -r "${HOME}/.ds/ds.sh" ] && source "${HOME}/.ds/ds.sh" "${HOME}/.ds" 1>&2'
 
 dsget () {
-    rm -rf "$HOME/.ds" > /dev/null 1>&2
-    wget 'https://github.com/stroparo/ds/archive/master.zip' -O "$HOME/.ds.zip" && \
-        unzip "$HOME/.ds.zip" -d "$HOME" && \
-        mv "$HOME/ds-master" "$HOME/.ds"
+    rm -rf "${INSTALL_DIR}" > /dev/null 1>&2
+    wget 'https://github.com/stroparo/ds/archive/master.zip' -O "${INSTALL_DIR}.zip" && \
+        unzip "${INSTALL_DIR}.zip" -d "$HOME" && \
+        mv "$HOME/ds-master" "${INSTALL_DIR}"
 }
 
 dsget || exit $?
 
-. "$HOME/.ds/ds.sh" "$HOME/.ds" >/dev/null 2>&1
+. "${INSTALL_DIR}/ds.sh" "${INSTALL_DIR}" >/dev/null 2>&1
 
 if [ -n "${DS_LOADED}" ] ; then
     appendunique "$DS_LOAD_CODE" "$HOME/.bashrc"
     appendunique "$DS_LOAD_CODE" "$HOME/.zshrc"
-    echo "INFO: Installed. Start a new terminal session." 1>&2
+    echo "INFO: DS installed." 1>&2
 else
-    echo "FATAL: ds not loaded." 1>&2
+    echo "FATAL: DS not loaded." 1>&2
     exit 99
 fi
 
