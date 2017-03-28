f01b0134af845977ce225c8c95934b252763fc4a
Cristian Stroparo <cstropz@gmail.com>
2017-03-27 22:13:17 -0300

    Formatting.

--- ds.sh
+++ ds.sh
@@ -12,14 +12,20 @@ DS_VERSION='DS version 0.2.0 - 2017-01-01 00:00'
 
 . "${DS_HOME}/ds00.sh" || return 10
 
-sourcefiles ${DS_VERBOSE:+-v} -q -t \
-    "${DS_HOME}/functions/*sh" \
-    "${DS_HOME}/ds0[1-9]*sh" \
-    "${DS_HOME}/ds[1-8][0-9]*sh" \
-    "${DS_HOME}/ds[A-Za-z]*sh"
+if [ -n "$DS_VERBOSE" ] ; then
+    echo 'Files to be sourced:' 1>&2
+    ls -l "${DS_HOME}/functions/"*sh
+    ls -l "${DS_HOME}/ds0"[1-9]*sh
+    ls -l "${DS_HOME}/ds"[1-8][0-9]*sh
+    ls -l "${DS_HOME}/ds"[A-Za-z]*sh
+    ls -l "${DS_HOME}/sshagent.sh"
+    ls -l "${DS_HOME}/ds99post.sh"
+fi
 
-echo ; sourcefiles ${DS_VERBOSE:+-v} \
-    "${DS_HOME}/sshagent.sh"
-
-sourcefiles ${DS_VERBOSE:+-v} -q -t \
-    "${DS_HOME}/ds99post.sh"
+sourcefiles ${DS_VERBOSE:+-v} -t "${DS_HOME}/functions/*sh"
+sourcefiles ${DS_VERBOSE:+-v} -t "${DS_HOME}/ds0[1-9]*sh"
+sourcefiles ${DS_VERBOSE:+-v} -t "${DS_HOME}/ds[1-8][0-9]*sh"
+sourcefiles ${DS_VERBOSE:+-v} -t "${DS_HOME}/ds[A-Za-z]*sh"
+echo
+sourcefiles ${DS_VERBOSE:+-v} "${DS_HOME}/sshagent.sh"
+sourcefiles ${DS_VERBOSE:+-v} -t "${DS_HOME}/ds99post.sh"
