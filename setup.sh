d1cf9408cd4e5f14f225b9033a8c84b5a53ac6d8
Cristian Stroparo <cstropz@gmail.com>
2017-04-10 14:44:15 -0300

    setup.sh makes the backup and restores on failure, otherwise removes it

--- setup.sh
+++ setup.sh
@@ -2,15 +2,20 @@
 
 INSTALL_DIR=${1:-$HOME/.ds}
 DS_LOAD_CODE='[ -r "${HOME}/.ds/ds.sh" ] && source "${HOME}/.ds/ds.sh" "${HOME}/.ds" 1>&2'
+BACKUP_FILENAME="${INSTALL_DIR}-$(date '+%y%m%d-%OH%OM%OS')"
 
-dsget () {
-    rm -rf "${INSTALL_DIR}" > /dev/null 1>&2
-    wget 'https://github.com/stroparo/ds/archive/master.zip' -O "${INSTALL_DIR}.zip" && \
-        unzip "${INSTALL_DIR}.zip" -d "$HOME" && \
-        mv "$HOME/ds-master" "${INSTALL_DIR}"
-}
+mv -f "${INSTALL_DIR}" "${BACKUP_FILENAME}"
+wget 'https://github.com/stroparo/ds/archive/master.zip' -O "${INSTALL_DIR}.zip" && \
+    unzip "${INSTALL_DIR}.zip" -d "$HOME" && \
+    mv "$HOME/ds-master" "${INSTALL_DIR}"
 
-dsget || exit $?
+if [ $? -ne 0 ] ; then
+    echo "FATAL: installation." 1>&2
+    mv -f "${BACKUP_FILENAME}" "${INSTALL_DIR}" && echo "INFO: backup restored." 1>&2
+    exit 1
+else
+    rm -rf "${BACKUP_FILENAME}"
+fi
 
 . "${INSTALL_DIR}/ds.sh" "${INSTALL_DIR}" >/dev/null 2>&1
 
