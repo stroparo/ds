ddfabd42e4a3d95e95bfb7b57dc4317b5bbbb327
Cristian Stroparo <Cristian Stroparo>
2017-12-04 10:32:33 -0200

    Fix

--- ds01setpath.sh
+++ ds01setpath.sh
@@ -11,3 +11,4 @@ pathmunge -x "${DS_HOME}" "$(ls -1 -d "$DS_HOME"/*/ | grep -v 'functions/$')"
 pathmunge -a -v 'EEPATH' -x "${DS_HOME}"
 
 chmodshells ${DS_VERBOSE:+-v} -a "${DS_HOME}" | grep -v retained 1>&2
+true
