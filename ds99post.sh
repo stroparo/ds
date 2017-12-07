2462becadfae15ad556555e7d9571ebdcbb5567d
Cristian Stroparo <Cristian Stroparo>
2017-12-07 09:47:05 -0200

    mkdir $DS_ENV_LOG if it does not exist

--- ds99post.sh
+++ ds99post.sh
@@ -6,16 +6,15 @@
 # ##############################################################################
 # Post file
 
-: ${DS_ENV_LOG:=$HOME/log}
-export DS_ENV_LOG
-
-export DS_GLOB="ds*sh aliases.sh ee.sh setup.sh sshagent.sh"
-
 DS_LOADED=true
 
-[ -n "${DS_VERBOSE}" ] && echo && dsinfo
+# Logging path:
+: ${DS_ENV_LOG:=$HOME/log} ; export DS_ENV_LOG
+[ -d "$DS_ENV_LOG" ] || mkdir -p "$DS_ENV_LOG" 2>/dev/null
 
-# Post-calls: Evaluate each line in the DS_POST_CALLS variable:
+# Post-calls
+# Evaluate each line in the DS_POST_CALLS variable:
+
 if [ -n "${DS_POST_CALLS}" ] ; then
     [ -n "$DS_VERBOSE" ] && echo "Running commands in DS_POST_CALLS ..." 1>&2
     while read nextcommand ; do
@@ -23,6 +22,11 @@ if [ -n "${DS_POST_CALLS}" ] ; then
     done <<EOF
 ${DS_POST_CALLS:-:}
 EOF
+fi
+
+# Display DS Information:
+if [ -n "${DS_VERBOSE}" ] ; then
+	echo && dsinfo
 fi
 
 return ${DS_POST_STATUS:-0}
