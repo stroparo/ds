d9e1d81b07950d8a3839961c4664cd7e33eba371
Cristian Stroparo <cstropz@gmail.com>
2018-01-05 18:00:30 -0200

    Fix

--- scripts/mountiso.sh
+++ scripts/mountiso.sh
@@ -1,9 +1,8 @@
-# DS - Daily Shells Library
-# License:
-#  See README.md document in projects page at
-#  https://github.com/stroparo/ds
+#!/usr/bin/env bash
 
-# #############################################################################
+# Daily Shells Library
+# More instructions and licensing at:
+# https://github.com/stroparo/ds
 
 mountiso () { sudo mount -o loop -t iso9660 "$@" ; }
 
