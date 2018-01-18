3e2fb4ef4fb6ec50128e0237ecb823458049669a
Cristian Stroparo <cstropz@gmail.com>
2018-01-18 07:22:40 -0600

    Split test functions in ds00.sh into functions/test*.sh

--- ds.sh
+++ ds.sh
@@ -2,12 +2,21 @@
 # More instructions and licensing at:
 # https://github.com/stroparo/ds
 
+# #############################################################################
+# Globals
+
 export DS_HOME="${1:-${HOME%/}/.ds}"
 
 export DS_CONF="${DS_HOME}/conf"
 export DS_VERSION='v0.4.0 2018-01-05'
 
+# #############################################################################
+# DS core
+
 . "${DS_HOME}/ds00.sh" || return 10
+
+# #############################################################################
+# Functions
 
 if [[ $DS_VERBOSE = vv ]] ; then
   sourcefiles -t -v "${DS_HOME}/functions/*sh"
@@ -15,9 +24,26 @@ else
   sourcefiles -t "${DS_HOME}/functions/*sh"
 fi
 
+# #############################################################################
+# DS additional core sources
+
 sourcefiles ${DS_VERBOSE:+-v} -t "${DS_HOME}/ds0[1-9]*sh"
 sourcefiles ${DS_VERBOSE:+-v} -t "${DS_HOME}/ds[1-8][0-9]*sh"
 sourcefiles ${DS_VERBOSE:+-v} -t "${DS_HOME}/ds[A-Za-z]*sh"
+
+# #############################################################################
+# Environments
+
 sourcefiles ${DS_VERBOSE:+-v} -t "${DS_HOME}/env*sh"
+
+# #############################################################################
+# Miscellaneous
+
 sourcefiles -v "${DS_HOME}/sshagent.sh"
+
+# #############################################################################
+# Post DS loading calls
+
 sourcefiles ${DS_VERBOSE:+-v} -t "${DS_HOME}/ds99post.sh"
+
+# #############################################################################
---
+++ functions/testshell.sh
@@ -0,0 +1,8 @@
+# Daily Shells Library
+# More instructions and licensing at:
+# https://github.com/stroparo/ds
+
+# #############################################################################
+# Shell testing routines
+
+_is_interactive () { [[ "$-" = *i* ]] ; }
