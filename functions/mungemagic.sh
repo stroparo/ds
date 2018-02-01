d24fbb4a23ac9855387b8e2587edc8573d4d2b9b
Cristian Stroparo <cstropz@gmail.com>
2018-01-31 23:46:27 -0200

    Rename mungebinlib -> mungemagic

--- functions/mungemagic.sh
+++ functions/mungemagic.sh
@@ -3,13 +3,18 @@
 #  See README.md document in projects page at
 #  https://github.com/stroparo/ds
 
-mungebinlib () {
+mungemagic () {
     # Munge descendant bin* and lib* directories to PATH and library variables.
     # Syn: {directory}
 
-    typeset mungeroot="${1}"
+    typeset mungeroot="$(cd "${1}"; echo "$PWD")"
     [ -e "$mungeroot" ] || return 1
 
+    if ls -d "$mungeroot"/*/ >/dev/null 2>&1 ; then
+	  for child in $(ls -d "$mungeroot"/*/); do
+	    pathmunge -x "$child"
+	  done
+	fi
     pathmunge -x $(find "$mungeroot" -name 'bin*' -type d)
     pathmunge -x -v LIBPATH $(find "$mungeroot" -name 'lib*' -type d)
     export LD_LIBRARY_PATH="${LIBPATH}${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
