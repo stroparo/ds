474eb8e3f9c7dd0a4beefd0c45c04bebaa599e60
Cristian Stroparo <cstropz@gmail.com>
2018-02-01 08:53:01 -0600

    Updated mungemagic was too slow, limit to .,bin,lib and children and their bin and lib

--- functions/mungemagic.sh
+++ functions/mungemagic.sh
@@ -10,13 +10,14 @@ mungemagic () {
     typeset mungeroot="$(cd "${1}"; echo "$PWD")"
     [ -e "$mungeroot" ] || return 1
 
+    pathmunge -x "$mungeroot"{,/bin}
+    pathmunge -x -v LIBPATH "$mungeroot"/lib
     if ls -d "$mungeroot"/*/ >/dev/null 2>&1 ; then
-	  for child in $(ls -d "$mungeroot"/*/); do
-	    pathmunge -x "$child"
-	  done
-	fi
-    pathmunge -x $(find "$mungeroot" -name 'bin*' -type d)
-    pathmunge -x -v LIBPATH $(find "$mungeroot" -name 'lib*' -type d)
+      for child in $(ls -d "$mungeroot"/*/); do
+        pathmunge -x "$child"{,/bin}
+        pathmunge -x -v LIBPATH "$child"/lib
+      done
+    fi
     export LD_LIBRARY_PATH="${LIBPATH}${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
 }
 
