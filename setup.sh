ef850dfebeca58dc37a2bed0ce1f452dbc02cd58
Cristian Stroparo <cstropz@gmail.com>
2018-02-02 06:02:07 -0600

    setup.sh -f (force) will make curl insecure (use in restrictive networks only)

--- setup.sh
+++ setup.sh
@@ -5,19 +5,42 @@
 # https://github.com/stroparo/ds
 
 # #############################################################################
-# Globals
+# Fixed globals
 
-INSTALL_DIR=${1:-$HOME/.ds}
+USAGE="[-h] [-f]"
+
 TEMP_DIR=$HOME
-BACKUP_FILENAME="${INSTALL_DIR}-$(date '+%y%m%d-%OH%OM%OS')"
+
 DS_LOAD_CODE='[ -r "${HOME}/.ds/ds.sh" ] && source "${HOME}/.ds/ds.sh" "${HOME}/.ds" 1>&2'
 DS_PKG_URL="https://github.com/stroparo/ds/archive/master.zip"
+
+
+# #############################################################################
+# Options
+
+OPTIND=1
+while getopts ':fh' option ; do
+  case "${option}" in
+    f) FORCE=true;;
+    h) echo "$USAGE"; exit;;
+  esac
+done
+shift "$((OPTIND-1))"
+
+# #############################################################################
+# Dynamic globals
+
+INSTALL_DIR=${1:-$HOME/.ds}
+BACKUP_FILENAME="${INSTALL_DIR}-$(date '+%y%m%d-%OH%OM%OS')"
 
 # Setup the downloader program (curl/wget)
 if which curl >/dev/null 2>&1 ; then
   export DLPROG=curl
   export DLOPT='-LSfs'
   export DLOUT='-o'
+  if ${FORCE:-false} ; then
+    export DLOPT="-k $DLOPT"
+  fi
 elif which wget >/dev/null 2>&1 ; then
   export DLPROG=wget
   export DLOPT=''
