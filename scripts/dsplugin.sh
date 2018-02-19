ab01b70276e9ec13fc29c1bfef42bb89334fc0b0
Cristian Stroparo <cstropz@gmail.com>
2018-02-19 11:17:37 -0600

    Fix dsplugin message

--- scripts/dsplugin.sh
+++ scripts/dsplugin.sh
@@ -122,7 +122,7 @@ EOF
       && cp -a "${repo_dir}"/* "${DS_HOME}/" \
       && rm -f -r "${repo_dir}" \
       && echo \
-      && echo "INFO: Plugin at '${repourl}' installed successfully" \
+      && echo "INFO: Plugin at '${repo_url}' installed successfully" \
       && echo
 
     if [ $? -ne 0 ] ; then
