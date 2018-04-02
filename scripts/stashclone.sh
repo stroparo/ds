23e4ac86bb1062703d3b61b00b4ebb76451f0ba9
Cristian Stroparo <cstropz@gmail.com>
2018-04-02 14:14:25 -0500

    Updated stashclone.sh to accept multiple repos

--- scripts/stashclone.sh
+++ scripts/stashclone.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export PROGNAME="stashclone.sh"
+
 stashclone () {
   # Syntax: {repo_host} {owner} {repo_name}
   # Options: [-d targetdir=$PWD/repo_name] [-u stash_user]
@@ -15,7 +17,8 @@ stashclone () {
   typeset stash_user working_repo_dir
 
   # Derived:
-  typeset repo_url_prefix
+  typeset repo_url_full repo_url_prefix
+  typeset repo_basename
 
   # Options:
   OPTIND=1
@@ -27,17 +30,24 @@ stashclone () {
   done
   shift "$((OPTIND-1))"
 
-  if [ $# -ne 3 ] ; then
-    echo "FATAL: Must have 3 args: hostname group|project repo-name" 1>&2
+  if [ $# -lt 3 ] ; then
+    echo "FATAL: Must have at least 3 args: hostname {group|project|user} [repos...]" 1>&2
     exit 1
   fi
 
   repo_host="$1"; shift
-  project_user="$2"; shift
+  project_user="$1"; shift
+
+  if [ -n "$working_repo_dir" ] && [ $# -gt 2 ] ; then
+    echo "${PROGNAME:+$PROGNAME: }FATAL: Cannot set a repo dir having more than one repo to be cloned." 1>&2
+    exit 1
+  fi
 
   for repo_name in "$@" ; do
 
-    repo_name="$(basename "${3%.git}")"
+    repo_name="$(basename "${repo_name%.git}")"
+
+    echo ${BASH_VERSION:+-e} "\n==> Repo '$repo_name'..."
 
     if [[ $repo_host = http*://* ]]; then
       if [[ $repo_host = http:* ]]; then
@@ -48,17 +58,17 @@ stashclone () {
 
     repo_url_prefix="${repo_proto}://${stash_user:+${stash_user}@}${repo_host}"
     repo_url_prefix="${repo_url_prefix}/stash/scm"
+    repo_url_full="${repo_url_prefix}/${project_user}/${repo_name}.git"
 
-    : ${working_repo_dir:=$PWD/$repo_name}
-
-    if [ -d "$working_repo_dir" ] ; then
-      echo "WARN: Repo already exists, nothing done ($working_repo_dir)" 1>&2
-    else
-      git clone --depth=1 \
-        "${repo_url_prefix}/${project_user}/${repo_name}.git" \
-        "${working_repo_dir}"
-    fi
+    echo "    URL: '${repo_url_full}'"
+    git clone --depth=1 "${repo_url_full}"
   done
+
+  if [ -n "$working_repo_dir" ] && [ $# -gt 2 ] ; then
+    repo_basename="${working_repo_dir##*/}"
+    repo_basename="${working_repo_dir%.git}"
+    mv "$repo_basename" "$working_repo_dir"
+  fi
 }
 
 stashclone "$@" || exit $?
