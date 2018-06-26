2695048806b7164bd2ea3bf46072e7e00474e831
Cristian Stroparo <cstropz@gmail.com>
2018-06-25 22:35:07 -0300

    Updated haltsafe echo message if truecrypt volumes still mounted and return 1 (error)

--- functions/haltsafe.sh
+++ functions/haltsafe.sh
@@ -1,19 +1,22 @@
 # DS - Daily Shells Library
 
 haltsafe () {
-    # Info: halts only if any truecrypt is unmounted correctly.
+  # Info: halts only if any truecrypt is unmounted correctly.
 
-    typeset tcdismounted=true
+  typeset tcdismounted=true
 
-    if which truecrypt >/dev/null 2>&1 && \
-        truecrypt -t -l && \
-        ! truecrypt -d
-    then
-        tcdismounted=false
-    fi
+  if which truecrypt >/dev/null 2>&1 && \
+    truecrypt -t -l && \
+    ! truecrypt -d
+  then
+    tcdismounted=false
+  fi
 
-    if ${tcdismounted:-false} ; then
-        sudo shutdown -h now
-    fi
+  if ${tcdismounted:-false} ; then
+    sudo shutdown -h now
+  else
+    echo "haltsafe: ERROR: truecrypt volumes still mounted." 1>&2
+    return 1
+  fi
 }
 
