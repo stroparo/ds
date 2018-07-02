e316690764e80bf9b3cba5dde58625ef5efe4f23
Cristian Stroparo <cstropz@gmail.com>
2018-07-01 23:12:05 -0300

    Added -a (after) option to mungemagic function pass it on to pathmunge call

--- functions/mungemagic.sh
+++ functions/mungemagic.sh
@@ -2,20 +2,21 @@
 
 mungemagic () {
     # Munge descendant bin* and lib* directories to PATH and library variables.
-    # Syn: {directory}
-
+    # Syn: [-a] {directory}
+    typeset optafter=""
+    if [ "$1" = "-a" ] ; then optafter="-a" ; shift ; fi
     typeset mungeroot="${1}"
     [ -d "$mungeroot" ] || return 1
 
     # Make mungeroot path canonical:
     mungeroot="$(cd "${mungeroot}"; echo "$PWD")"
 
-    pathmunge -x "$mungeroot"{,/bin}
-    pathmunge -x -v LIBPATH "$mungeroot"/lib
+    pathmunge ${optafter} -x "$mungeroot"{,/bin}
+    pathmunge ${optafter} -x -v LIBPATH "$mungeroot"/lib
     if ls -d "$mungeroot"/*/ >/dev/null 2>&1 ; then
       for child in $(ls -d "$mungeroot"/*/); do
-        pathmunge -x "$child"{,/bin}
-        pathmunge -x -v LIBPATH "$child"/lib
+        pathmunge ${optafter} -x "$child"{,/bin}
+        pathmunge ${optafter} -x -v LIBPATH "$child"/lib
       done
     fi
     export LD_LIBRARY_PATH="${LIBPATH}${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
