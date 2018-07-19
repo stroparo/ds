f0b34955ccab0fa9e178983b6d4e37adb7e2873d
Cristian Stroparo <cstropz@gmail.com>
2018-07-19 14:15:45 -0500

    no need for while read loop in dsbulk function

--- functions/dsdiskusage.sh
+++ functions/dsdiskusage.sh
@@ -6,14 +6,13 @@
 # Function dubulk - Displays disk usage of filenames read from stdin.
 #  Handles massive file lists.
 dubulk () {
-    while read filename ; do echo "${filename}" ; done \
-    | xargs -n 1000 du -sm
+  xargs -I{} -n 1000 du -sm -- {}
 }
 
 # Function dudesc - Displays disk usage of filenames read from stdin.
 #  Sorted in descending order.
 dudesc () {
-    dubulk | sort -rn
+  dubulk | sort -rn
 }
 
 # Function dufile - Process data formatted from du, from stdin,
@@ -22,29 +21,28 @@ dudesc () {
 # Use case #1: pass filenames to another process that
 #  must act on a filesize ordered sequence.
 dufile () {
-    sed -e 's#^[^[:blank:]]*[[:blank:]][[:blank:]]*##'
+  sed -e 's#^[^[:blank:]]*[[:blank:]][[:blank:]]*##'
 }
 
 # Function dugt1 - Displays disk usage of filenames read from stdin which are greater than 1MB.
 dugt1 () {
-    dubulk | sed -n -e '/^[1-9][0-9]*[.]/p'
+  dubulk | sed -n -e '/^[1-9][0-9]*[.]/p'
 }
 
 # Function dugt1desc - Displays disk usage of filenames read from stdin which are greater than 1MB.
 #  Sorted in descending order.
 dugt1desc () {
-    dubulk | sed -n -e '/^[1-9][0-9]*[.]/p' | sort -rn
+  dubulk | sed -n -e '/^[1-9][0-9]*[.]/p' | sort -rn
 }
 
 # Function dugt10 - Displays disk usage of filenames > 10MBm read from stdin.
 #  Sorted in descending order.
 dugt10 () {
-    dubulk | sed -n -e '/^[1-9][0-9][0-9]*[.]/p'
+  dubulk | sed -n -e '/^[1-9][0-9][0-9]*[.]/p'
 }
 
 # Function dugt10desc - Displays disk usage of filenames > 10MBm read from stdin.
 #  Sorted in descending order.
 dugt10desc () {
-    dubulk | sed -n -e '/^[1-9][0-9][0-9]*[.]/p' | sort -rn
+  dubulk | sed -n -e '/^[1-9][0-9][0-9]*[.]/p' | sort -rn
 }
-
