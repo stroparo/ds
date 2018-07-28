a03a67480369da5b2c14ab20010bc9444927c577
Cristian Stroparo <cstropz@gmail.com>
2018-07-28 00:35:24 +0000

    cbssh function return cbssh.sh's exit code on error

--- functions/cbssh.sh
+++ functions/cbssh.sh
@@ -2,5 +2,5 @@
 
 cbssh () {
     # Info: Copies ~/.ssh/id_rsa.pub contents to the clipboard via the DS cb script
-    cb.sh < "${HOME}/.ssh/id_rsa.pub"
+    cb.sh < "${HOME}/.ssh/id_rsa.pub" || return $?
 }
