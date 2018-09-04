d289db5261e2d509b9a4f349dfd858d77cb5f24e
Cristian Stroparo <cstropz@gmail.com>
2018-09-03 19:19:27 -0500

    Fix eecp* functions to use eeport for the -P (port) option

--- functions/ee.sh
+++ functions/ee.sh
@@ -25,8 +25,8 @@
 # eecmd='some command'
 # eeid='some .pem or other file to be handled to ssh -i option'
 
-eecpfrom () { eesel "${1%%:*}" ; scp "${ee}:${1##*:}" "$2" ; }
-eecpto () { eesel "${2%%:*}" ; scp "$1" "${ee}:${2##*:}" ; }
+eecpfrom () { eesel "${1%%:*}" ; scp -P ${eeport:-22} "${ee}:${1##*:}" "$2" ; }
+eecpto () { eesel "${2%%:*}" ; scp -P ${eeport:-22} "$1" "${ee}:${2##*:}" ; }
 
 eeauth () {
 
