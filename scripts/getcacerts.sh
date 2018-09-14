68a88eb7450ad4f526e6d2ef6a2e9e661941ada2
Cristian Stroparo <cstropz@gmail.com>
2018-09-13 20:10:11 -0500

    getcacerts script allows for specifying certs dirs defaulting this arg list to /usr/ssl/certs

--- scripts/getcacerts.sh
+++ scripts/getcacerts.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
 getcacerts () {
-  curl -L "http://curl.haxx.se/ca/cacert.pem" \
-    | awk '
-        split_after==1 {n++;split_after=0}
-        /-----END CERTIFICATE-----/ {split_after=1}
-        {print > "cert" n ".pem"}'
-  c_rehash || sudo c_rehash
+  for certdir in "$@" ; do
+    (
+      mkdir /usr/ssl/certs >/dev/null 2>&1 \
+        || sudo mkdir /usr/ssl/certs >/dev/null 2>&1
+      cd /usr/ssl/certs
+      curl -LSfs "http://curl.haxx.se/ca/cacert.pem" \
+        | awk '
+            split_after==1 {n++;split_after=0}
+            /-----END CERTIFICATE-----/ {split_after=1}
+            {print > "cert" n ".pem"}'
+    )
+    c_rehash "$certdir" || sudo c_rehash
+  done
 }
 
-getcacerts
+if [ $# -eq 0 ] ; then
+  getcacerts "/usr/ssl/certs"
+else
+  getcacerts "$@"
+fi
