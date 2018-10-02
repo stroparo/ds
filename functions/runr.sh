c9030b11434cc39bb09ff0c77c4f24b471f1d1de
Cristian Stroparo <stroparo@outlook.com>
2018-10-02 10:02:15 -0300

    New runr function

---
+++ functions/runr.sh
@@ -0,0 +1,10 @@
+runr () {
+  if [ -f ~/.runr/entry.sh ] ; then
+    [ -x ~/.runr/entry.sh ] || chmod 755 ~/.runr/entry.sh
+    ~/.runr/entry.sh -p "$@"
+  else
+    bash -c "$(curl -LSf "https://bitbucket.org/stroparo/runr/raw/master/entry.sh" \
+      || curl -LSf "https://raw.githubusercontent.com/stroparo/runr/master/entry.sh")" \
+      entry.sh "$@"
+  fi
+}
