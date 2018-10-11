44b68028f4562cd66170305b5eec7d0240ffd9b2
Cristian Stroparo <stroparo@outlook.com>
2018-10-11 14:19:09 -0500

    Update docker functions

--- functions/dockerfunctions.sh
+++ functions/dockerfunctions.sh
@@ -5,11 +5,19 @@ dockeraccess () {
 }
 
 dockercleancontainers () {
-  docker stop $(docker ps -q) && docker rm $(docker ps -a -q -f status=exited)
+  docker stop $(docker ps -q) && docker rm $(docker ps -a -q -f status=exited --no-trunc)
 }
 
 dockercleanimages () {
-  docker rmi $(docker images -q -f dangling=true)
+  docker rmi $(docker images -q -f dangling=true --no-trunc)
+}
+
+dockercleanimagesnone () {
+  docker rmi $(docker images | grep "none" | awk '/ / { print $3 }')
+}
+
+dockercleannetworks () {
+  docker network rm $(docker network ls | grep "bridge" | awk '/ / { print $1 }')
 }
 
 dockercleanvolumes () {
