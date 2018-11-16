6f7ded57307add0eea5db09a6ce30078098b99df
Cristian Stroparo <stroparo@outlook.com>
2018-11-16 08:38:17 -0200

    Disown startdropbox function

--- functions/services.sh
+++ functions/services.sh
@@ -1,5 +1,5 @@
 startdropbox () {
   if [ -x ~/.dropbox-dist/dropboxd ] && ! pgrep -fl dropbox ; then
-    env DBUS_SESSION_BUS_ADDRESS='' ~/.dropbox-dist/dropboxd
+    env DBUS_SESSION_BUS_ADDRESS='' ~/.dropbox-dist/dropboxd & disown
   fi
 }
