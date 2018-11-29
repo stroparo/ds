923a6b2f31850d377023b69edaaa1668f43b5bd0
Cristian Stroparo <stroparo@outlook.com>
2018-11-28 22:42:56 -0200

    runrnopreserve function

--- functions/runr.sh
+++ functions/runr.sh
@@ -8,3 +8,14 @@ runr () {
       entry.sh "$@"
   fi
 }
+
+runrnopreserve () {
+  if [ -f ~/.runr/entry.sh ] ; then
+    [ -x ~/.runr/entry.sh ] || chmod 755 ~/.runr/entry.sh
+    ~/.runr/entry.sh "$@"
+  else
+    bash -c "$(curl -LSf "https://bitbucket.org/stroparo/runr/raw/master/entry.sh" \
+      || curl -LSf "https://raw.githubusercontent.com/stroparo/runr/master/entry.sh")" \
+      entry.sh "$@"
+  fi
+}
