a2a6d3b2747490b5b7d1dbc42f38084350d5978b
Cristian Stroparo <stroparo@outlook.com>
2018-12-12 08:40:47 -0600

    New runru function that backs up and reinstall runr before running it

--- functions/runr.sh
+++ functions/runr.sh
@@ -1,10 +1,32 @@
 runr () {
+  typeset ignore_ssl_option
+  typeset script_content
+
+  if ${IGNORE_SSL:-false} ; then
+    ignore_ssl_option='-k'
+  fi
+
   if [ -f ~/.runr/entry.sh ] ; then
-    [ -x ~/.runr/entry.sh ] || chmod 755 ~/.runr/entry.sh
-    ~/.runr/entry.sh "$@"
+    script_content="$(cat ~/.runr/entry.sh)"
+    bash -c "${script_content}" entry.sh "$@"
   else
-    bash -c "$(curl -LSf "https://bitbucket.org/stroparo/runr/raw/master/entry.sh" \
-      || curl -LSf "https://raw.githubusercontent.com/stroparo/runr/master/entry.sh")" \
+    bash -c "$(curl ${ignore_ssl_option} -LSf "https://bitbucket.org/stroparo/runr/raw/master/entry.sh" \
+      || curl ${ignore_ssl_option} -LSf "https://raw.githubusercontent.com/stroparo/runr/master/entry.sh")" \
       entry.sh "$@"
   fi
+}
+
+runru () {
+
+  typeset ignore_ssl_option
+
+  if ${IGNORE_SSL:-false} ; then
+    ignore_ssl_option='-k'
+  fi
+
+  mv ~/.runr ~/.runr.$(date '+%Y%m%d-%OH%OM%OS') || return $?
+
+  bash -c "$(curl ${ignore_ssl_option} -LSf "https://bitbucket.org/stroparo/runr/raw/master/entry.sh" \
+    || curl ${ignore_ssl_option} -LSf "https://raw.githubusercontent.com/stroparo/runr/master/entry.sh")" \
+    entry.sh "$@"
 }
