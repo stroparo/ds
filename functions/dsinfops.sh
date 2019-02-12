d54c77512906777bab4c14127c8c79bec0500e97
Cristian Stroparo <stroparo@outlook.com>
2019-02-12 17:13:56 -0600

    Rename

--- functions/dsinfops.sh
+++ functions/dsinfops.sh
@@ -1,9 +1,16 @@
 # DS - Daily Shells Library
 
-# #############################################################################
-# Ops routines
+# Process management routines
 
 pgr () { ps -ef | egrep -i "$1" | egrep -v "grep.*(${1})" ; }
 psfu () { ps -fu "${UID:-$(id -u)}" -U "${UID:-$(id -u)}" ; }
-psnoshells () { ps -ef | grep -v bash | grep -v zsh | grep -v sshd ; }
 psuser () { ps -ef | grep "^${USER}" ; }
+
+psnoshells () {
+  ps -ef \
+    | grep -v bash \
+    | grep -v zsh \
+    | grep -v sshd \
+    | egrep -i "$1" \
+    | egrep -v "grep.*(${1})"
+}
