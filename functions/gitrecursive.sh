90118f3706db0ea653b8bd8ab495cab2cfb46f13
Cristian Stroparo <stroparo@outlook.com>
2019-02-19 08:25:52 -0600

    Rename rfetchallp to rfetchallprune

--- functions/gitrecursive.sh
+++ functions/gitrecursive.sh
@@ -18,7 +18,7 @@ rci     ()  { GITR_PARALLEL=false gitr.sh -fv -- commit -m "'$@'" ; }
 rco     ()  { GITR_PARALLEL=false gitr.sh -fv -- checkout "$@" ; }
 rdca    ()  { GITR_PARALLEL=false gitr.sh -f  -- diff --cached "$@" ; }
 rfetch  ()  { gitr.sh -f${GITR_VERBOSE_OPTION} -- fetch "$@" ; }
-rfetchallp () { gitr.sh -f${GITR_VERBOSE_OPTION} -- fetch --all -p "$@" ; }
+rfetchallprune () { gitr.sh -f${GITR_VERBOSE_OPTION} -- fetch --all -p "$@" ; }
 rpull   ()  { gitr.sh -f${GITR_VERBOSE_OPTION} -- pull "$@" ; }
 rpush   ()  { gitr.sh -f${GITR_VERBOSE_OPTION} -- push "$@" ; }
 rpushmirror () { gitr.sh -fv push mirror ${1:-master} | egrep -v "fatal:|make sure|repository exists|^$" ; }
@@ -27,4 +27,3 @@ rss     ()  { gitr.sh -f${GITR_VERBOSE_OPTION} -- status -s "$@" ; }
 # Compound commands
 rpushcurrent () { rpush origin HEAD ; rpushmirror HEAD ; rss ; }
 rpushmatching () { rpush origin : ; rpushmirror : ; rss ; }
-rsyncmaster () { rco master && rpull origin master && rpush origin master && rpushmirror master ; rss ; }
