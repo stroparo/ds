17deaea10bd68d7293578d33c4cb57594546c67b
Cristian Stroparo <stroparo@outlook.com>
2019-07-26 11:25:23 -0500

    Update gitpullmaster function better visual formatting.

--- functions/gitpullmaster.sh
+++ functions/gitpullmaster.sh
@@ -1,11 +1,15 @@
 gitpullmaster () {
+  echo
   for repo in "$@" ; do
     if [ -d "$repo" ] ; then
       (
         cd "$repo"
+        if [ ! -d "$repo/.git" ] ; then continue ; fi
+        echo
+        echo "==> $(basename "$repo")"
         pwd
+        echo
         git pull origin master
-        # TODO add push to mirror remote (when it exists)
         git branch -vv
         echo "git status at '${PWD}':"
         git status -s
