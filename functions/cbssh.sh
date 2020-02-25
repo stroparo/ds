2911f88697b93b7f3d68ee2755c7eb3b83018d54
Cristian Stroparo <stroparo@outlook.com>
2020-02-24 23:26:38 -0300

    Skip cbssh function if id_rsa.pub does not exist in ~/.ssh

--- functions/cbssh.sh
+++ functions/cbssh.sh
@@ -1,6 +1,13 @@
 # DS - Daily Shells Library
 
 cbssh () {
+
     # Info: Copies ~/.ssh/id_rsa.pub contents to the clipboard via the DS cb script
+
+    if [ ! -e "${HOME}/.ssh/id_rsa.pub" ] ; then
+      echo "${PROGNAME:+$PROGNAME: }SKIP: '${HOME}/.ssh/id_rsa.pub' does not exist." 1>&2
+      return
+    fi
+
     cb.sh < "${HOME}/.ssh/id_rsa.pub" || return $?
 }
