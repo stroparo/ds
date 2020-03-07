67ebcdb8ceba8af884a9e6de446dad78b36d9e53
Cristian Stroparo <stroparo@outlook.com>
2020-03-06 22:04:26 -0600

    Tune findscripts.sh

--- scripts/findscripts.sh
+++ scripts/findscripts.sh
@@ -1,11 +1,9 @@
-#!/usr/bin/env bash
-
-# DS - Daily Shells Library
+#!/bin/bash
 
 # Info: Finds scripts in root dirs passed as arguments.
 
-typeset re_shells='perl|python|ruby|sh'
+if [ $# -eq 0 ] ; then exit ; fi
 
-awk 'FNR == 1 && $0 ~ /^#!.*('"$re_shells"') */ { print FILENAME; }
-  FNR > 1 { nextfile; }' \
-  $(find "$@" -type f | egrep -v '[.](git|hg|svn)')
+for dir in "$@" ; do
+  find "${dir}" -type f \( -name '*.sh' -o -name '*.[ck]sh' -o -name '*.pl' -o -name '*.py' -o -name '*.rb' \)
+done
