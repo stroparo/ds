edc039291d639c29d9aa0c0e5a605c14a5bef249
Cristian Stroparo <stroparo@outlook.com>
2020-03-06 20:25:49 -0600

    Tune

--- envvars.sh
+++ envvars.sh
@@ -3,9 +3,17 @@
 #  See README.md document in projects page at
 #  https://github.com/stroparo/ds
 
-# System installers
-export APTPROG=apt-get; which apt >/dev/null 2>&1 && export APTPROG=apt
-export RPMPROG=yum; which dnf >/dev/null 2>&1 && export RPMPROG=dnf
-export RPMGROUP="yum groupinstall"; which dnf >/dev/null 2>&1 && export RPMGROUP="dnf group install"
-export INSTPROG="$APTPROG"; which "$RPMPROG" >/dev/null 2>&1 && export INSTPROG="$RPMPROG"
-true # nullify previous command shortcircuiting to false
+export APTPROG=apt
+
+export RPMPROG=yum
+export RPMGROUP="yum groupinstall"
+if [ -e /sbin/dnf ] ; then
+  export RPMPROG=dnf
+  export RPMGROUP="dnf group install"
+fi
+
+if [ -e /sbin/apt ] ; then
+  export INSTPROG="$APTPROG"
+else
+  export INSTPROG="$RPMPROG"
+fi
