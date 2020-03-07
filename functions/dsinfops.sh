436d0fc9f80a63426dcf4bee7dea27b6d8ae598b
Cristian Stroparo <stroparo@outlook.com>
2020-03-06 22:04:42 -0600

    Rename psnoshells() to psnosh

--- functions/dsinfops.sh
+++ functions/dsinfops.sh
@@ -6,7 +6,7 @@ pgr () { ps -ef | egrep -i "$1" | egrep -v "grep.*(${1})" ; }
 psfu () { ps -fu "${UID:-$(id -u)}" -U "${UID:-$(id -u)}" ; }
 psuser () { ps -ef | grep "^${USER}" ; }
 
-psnoshells () {
+psnosh () {
   ps -ef \
     | egrep -v 'bash|zsh|sshd' \
     | egrep -i "${1}" \
