cc4a45696cec37cc46663f9cbffd3b1e2bbaed3d
Cristian Stroparo <stroparo@outlook.com>
2020-03-09 14:09:50 -0500

    Format

--- ds10varinstprog.sh
+++ ds10varinstprog.sh
@@ -1,19 +1,5 @@
-# DS - Daily Shells Library
-# License:
-#  See README.md document in projects page at
-#  https://github.com/stroparo/ds
-
 export APTPROG=apt
-
 export RPMPROG=yum
 export RPMGROUP="yum groupinstall"
-if [ -e /sbin/dnf ] ; then
-  export RPMPROG=dnf
-  export RPMGROUP="dnf group install"
-fi
-
-if [ -e /sbin/apt ] ; then
-  export INSTPROG="$APTPROG"
-else
-  export INSTPROG="$RPMPROG"
-fi
+if [ -e /sbin/dnf ] ; then export RPMPROG=dnf ; export RPMGROUP="dnf group install"; fi
+if [ -e /sbin/apt ] ; then export INSTPROG="$APTPROG" ; else export INSTPROG="$RPMPROG"; fi
