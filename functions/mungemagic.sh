d5aca83232b521824a846bf776c28ca94db43a25
Cristian Stroparo <stroparo@outlook.com>
2020-03-31 22:15:25 -0500

    Update mungemagic to munge also mungeroot's children

--- functions/mungemagic.sh
+++ functions/mungemagic.sh
@@ -11,8 +11,18 @@ mungemagic () {
     # Make mungeroot path canonical:
     mungeroot="$(cd "${mungeroot}"; echo "$PWD")"
 
+    if [ -n "${optafter}" ] ; then
+      pathmunge ${optafter} -x $(ls -1d "$mungeroot"/*/ | egrep -v -w 'bin|lib'))
+    fi
+
     pathmunge ${optafter} -x $(echo $(find "$mungeroot" -type d -maxdepth 2 -name bin))
     pathmunge ${optafter} -x -v LIBPATH $(echo $(find "$mungeroot" -type d -maxdepth 2 -name lib))
-    export LD_LIBRARY_PATH="${LIBPATH}${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
-}
 
+    # Mirror LIBPATH into LD_LIBRARY_PATH:
+    if [ -n "${optafter}" ] ; then
+      export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+${LD_LIBRARY_PATH}:}${LIBPATH}"
+    else
+      pathmunge -x $(ls -1d "$mungeroot"/*/ | egrep -v -w 'bin|lib')
+      export LD_LIBRARY_PATH="${LIBPATH}${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
+    fi
+}
