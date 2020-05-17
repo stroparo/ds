467911bf844b239e38e3a80b578da612ffc0b111
Cristian Stroparo <stroparo@outlook.com>
2020-05-17 15:45:01 -0300

    Streamline compresslogs script and add rootdir option in -d root

--- scripts-media/subtitle_shift.py
+++ scripts-media/subtitle_shift.py
@@ -4,8 +4,10 @@ import time
 
 lines = [ line for line in fileinput.input() ]
 
-
+# Test:
 base = time.mktime(time.strptime('00:01:02', '%H:%M:%S'))
 secs = 120
 res = time.strftime('%H:%M:%S', time.localtime(base + secs))
 print(res)
+
+# TODO
