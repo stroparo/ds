c790637c536faec2720a25fbb04fe98111800d9a
Cristian Stroparo <stroparo@outlook.com>
2020-05-26 14:51:09 -0300

    Fix aptinstallppa repo check

--- scripts-deb/aptinstallppa.sh
+++ scripts-deb/aptinstallppa.sh
@@ -6,7 +6,7 @@ PKG="$2"
 if ! egrep -i -q -r 'ubuntu' /etc/*release ; then exit ; fi
 export APTPROG=apt-get; which apt >/dev/null 2>&1 && export APTPROG=apt
 
-if ! ls -d *(echo "${PPA}" | sed -e 's/[/-].*//')* >/dev/null 2>&1 ; then
+if ! (ls -d /etc/apt/sources.list.d/* | fgrep -q "$(echo "${PPA}" | sed -e 's/[/-].*//')") ; then
   sudo add-apt-repository -y "ppa:${PPA}"
 fi
 
