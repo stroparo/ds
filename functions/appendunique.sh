3ccdf54207b320e681cbfef82f34fdbab1fbc42a
Cristian Stroparo <stroparo@outlook.com>
2020-05-26 22:07:26 -0300

    Refactor appendunique

--- functions/appendunique.sh
+++ functions/appendunique.sh
@@ -1,23 +1,16 @@
 appendunique () {
-    # Syntax: [-n] string file1 [file2 ...]
-    [ "$1" = '-n' ] && shift && typeset newline=true
-    [ -z "$1" ] && return 0
-    typeset result=0
-    typeset text="${1}" ; shift
-    for f in "$@" ; do
-        if [ ! -e "$f" ] ; then
-            result=1
-            echo "ERROR '${f}' does not exist" 1>&2
-            continue
-        fi
-        if ! grep -F -q "$text" "$f" ; then
-            ${newline:-false} && echo ${BASH_VERSION:+-e} '\n' >> "$f"
-            if ! echo "$text" >> "$f" ; then
-                result=1
-                echo "ERROR appending '$f'" 1>&2
-            fi
-        fi
-    done
-    return ${result}
+  # Syntax: [-n] string file1 [file2 ...]
+  if [ "$1" = '-n' ] ; then shift ; typeset newline=true; fi
+  typeset result=0
+  typeset text="${1}" ; shift
+  if [ -z "${text}" ] ; then return 0 ; fi
+  for f in "$@" ; do
+    if [ ! -e "$f" ] ; then echo "WARN: '${f}' does not exist." 1>&2 ; continue ; fi
+    if ! grep -F -q "$text" "$f" ; then
+      if ${newline:-false} ; then echo '' >> "$f" ; fi
+      if ! echo "$text" >> "$f" ; then result=1 ; echo "ERROR: appending '$f'" 1>&2 ; fi
+    fi
+  done
+  return ${result}
 }
 
