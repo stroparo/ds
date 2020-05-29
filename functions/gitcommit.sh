9f2b74585ac80075f5ca5caac66ed12733324876
Cristian Stroparo <stroparo@outlook.com>
2020-05-29 11:43:00 -0300

    Improve git commit functions and make them more robust by returning an error if any of the subcommands fails

--- functions/gitcommit.sh
+++ functions/gitcommit.sh
@@ -2,11 +2,11 @@
 
 gci () { typeset msg="$1"; shift; git commit -m "$msg" "$@" ; }
 gciadd () { typeset msg="$1"; shift; git commit -m "Add $msg" "$@" ; }
-gciarr () { typeset msg="$1"; shift; git commit -m "Rearrange $msg" "$@" ; }
 gcicom () { typeset msg="$1"; shift; git commit -m "Comment $msg" "$@" ; }
 gcifix () { typeset msg="$1"; shift; git commit -m "Fix $msg" "$@" ; }
 gcifmt () { typeset msg="$1"; shift; git commit -m "Format $msg" "$@" ; }
 gcimv () { typeset msg="$1"; shift; git commit -m "Move $msg" "$@" ; }
+gciorg () { typeset msg="$1"; shift; git commit -m "Organize $msg" "$@" ; }
 gcirf () { typeset msg="$1"; shift; git commit -m "Refactor $msg" "$@" ; }
 gcirn () { typeset msg="$1"; shift; git commit -m "Rename $msg" "$@" ; }
 gcirm  () { typeset msg="$1"; shift; git commit -m "Remove $msg" "$@" ; }
@@ -15,11 +15,11 @@ gciup () { typeset msg="$1"; shift; git commit -m "Update $msg" "$@" ; }
 gciwp () { typeset msg="$1"; shift; git commit -m "Work in progress $msg" "$@" ; }
 
 gpi () { gci "$@" ; gpa ; }
-gpiarr () { gciarr "$@" ; gpa ; }
 gpicom () { gcicom "$@" ; gpa ; }
 gpifix () { gcifix "$@" ; gpa ; }
 gpifmt () { gcifmt "$@" ; gpa ; }
 gpimv () { gcimv "$@" ; gpa ; }
+gpiorg () { gciorg "$@" ; gpa ; }
 gpirf () { gcirf "$@" ; gpa ; }
 gpirn () { gcirn "$@" ; gpa ; }
 gpirm  () { gcirm "$@" ; gpa ; }
@@ -37,8 +37,8 @@ g1 () {
   git status -s
 
   echo
-  echo "Diff:"
-  git diff
+  echo "Diff index (staged) vs HEAD ie what is being commited:"
+  git diff --ignore-space-at-eol --cached
 
   echo
   if userconfirm "Commit and push?" ; then
@@ -49,29 +49,39 @@ g1 () {
     done
 
     git add -A "$@"
+
+    # If explicit args then ensure all removed files are also staged for removal:
+    if [ $# -gt 0 ] && [ ! -z "$*" ] ; then
+      git rm $(git ls-files --deleted) 2> /dev/null
+    fi
+
     git commit -m "$message"
 
     gpa HEAD
-    gpa HEAD
+    return $?
   fi
 }
 
 
 gpa () {
   # Info: Git push the given branch to all remotes (branch defaults to HEAD)
-  # Syn: [branch=HEAD]
+  # Usage: [branch=HEAD]
 
   typeset branch="${1:-HEAD}"
   typeset remote
+  typeset result=0
 
   echo
   echo "gpa: INFO: Checking out the '${branch}' branch..."
-  git checkout "${branch}"
+  if ! git checkout "${branch}" ; then return 1 ; fi
 
   for remote in $(git remote) ; do
     echo
     echo "gpa: INFO: Pushing to remote '${remote}'s branch '${branch}'..."
-    git push "${remote}" "${branch}"
+    if ! git push "${remote}" "${branch}" ; then
+      result=1
+    fi
   done
+  return $result
 }
 
