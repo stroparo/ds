b882af11f2e42ea1f0bd8389015e9685aa5bb1ce
Cristian Stroparo <stroparo@outlook.com>
2020-05-31 15:22:08 -0300

    Update runr functions

--- functions/runr.sh
+++ functions/runr.sh
@@ -1,8 +1,9 @@
 # RUNR solution (stroparo/runr) wrappers
 
-# runr - prefer it as a function instead of an
-#  alias, so scripted subshells will have it:
+# Prefer these as functions over aliases so scripted subshells will have access:
+
 unalias runr >/dev/null 2>&1
 runr () { "${DS_HOME:-$HOME/.ds}"/scripts/runr.sh "$@" ; }
 
+unalias runrup >/dev/null 2>&1
 runrup () { "${DS_HOME:-$HOME/.ds}"/scripts/runr.sh -u "$@" ; }
