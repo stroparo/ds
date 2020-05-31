d87e2e9e201f4c55bc68517096c96eb83fa621d1
Cristian Stroparo <stroparo@outlook.com>
2020-05-31 20:23:18 -0300

    Update runr script debug to trigger upon $RUNR_DEBUG true

--- scripts/runr.sh
+++ scripts/runr.sh
@@ -1,25 +1,30 @@
 #!/usr/bin/env bash
 
 PROGNAME="runr.sh"
+if ${IGNORE_SSL:-false} ; then IGNORE_SSL_OPTION='-k'; fi
 
 
 _get_entry_online () {
-  bash -c "$(curl ${ignore_ssl_option} ${DLOPTEXTRA} -LSf "https://bitbucket.org/stroparo/runr/raw/master/entry.sh" \
-    || curl ${ignore_ssl_option} ${DLOPTEXTRA} -LSf "https://raw.githubusercontent.com/stroparo/runr/master/entry.sh")"
+  bash -c "$(curl ${IGNORE_SSL_OPTION} ${DLOPTEXTRA} -LSf "https://bitbucket.org/stroparo/runr/raw/master/entry.sh" \
+    || curl ${IGNORE_SSL_OPTION} ${DLOPTEXTRA} -LSf "https://raw.githubusercontent.com/stroparo/runr/master/entry.sh")"
 }
 
 
 _runr () {
-  typeset ignore_ssl_option
   typeset runr_entry_code
 
-  if ${IGNORE_SSL:-false} ; then
-    ignore_ssl_option='-k'
+  if ${RUNR_DEBUG:-false} && [ -f "${DEV}/runr/entry.sh" ] ; then
+    if [ -d "${HOME}/.runr" ] && ! cp -p -r "${HOME}/.runr" "${HOME}/.runr-$(date '+%Y%m%d-%OH%OM')" ; then
+      echo "${PROGNAME:+$PROGNAME: }FATAL: Could not backup '${HOME}/.runr'." 1>&2
+      return 1
+    fi
+    if ! rm -f -r "${HOME}/.runr" || ! cp -r -v "${DEV}/runr" "${HOME}/.runr" ; then
+      echo "${PROGNAME:+$PROGNAME: }FATAL: Could copy '${DEV}/runr' to '${HOME}/.runr'." 1>&2
+      return 1
+    fi
   fi
 
-  if ${DS_DEBUG:-false} && [ -f "$DEV/runr/entry.sh" ] ; then
-    runr_entry_code="$(cat "$DEV/runr/entry.sh")"
-  elif [ -f "${HOME}/.runr/entry.sh" ] ; then
+  if [ -f "${HOME}/.runr/entry.sh" ] ; then
     runr_entry_code="$(cat "${HOME}/.runr/entry.sh")"
   else
     runr_entry_code="$(_get_entry_online)"
@@ -35,4 +40,5 @@ _runr () {
 
 
 _runr "$@"
+exit $?
 
