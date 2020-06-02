f2d448572c3622bcdb65ff34bbfb82e6f45c81bc
Cristian Stroparo <stroparo@outlook.com>
2020-06-01 21:05:17 -0300

    Format

--- scripts/runrhash.sh
+++ scripts/runrhash.sh
@@ -2,19 +2,18 @@
 
 PROGNAME="runrhash.sh"
 
-: ${DEV:=$HOME/workspace}
-
 # Purpose:
 # Rehash runr repository from the workspace and
 # seize the moment to also run any recipes in
 # arguments, if any.
 
+: ${DEV:=$HOME/workspace}
 
 unset ignore_ssl_option
-
 if ${IGNORE_SSL:-false} ; then
   ignore_ssl_option='-k'
 fi
+
 
 if [ -d ~/.runr ] && ! mv -v ~/.runr ~/.runr.$(date '+%Y%m%d-%OH%OM%OS') ; then
   echo "${PROGNAME:+$PROGNAME: }FATAL: Could not backup '${HOME}/.runr'." 1>&2
