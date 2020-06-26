c39c411551877d72e7219ccc395e602a6cf7910f
Cristian Stroparo <stroparo@outlook.com>
2020-06-26 18:04:37 -0300

    Improve dsplug()

--- functions/dsplug.sh
+++ functions/dsplug.sh
@@ -1,5 +1,9 @@
 dsplug () {
-  if dsplugin.sh "$@" ; then
-    dsload
-  fi
+  for plugin in "$@" ; do
+    plugin_basename="${plugin%.git}"
+    plugin_basename="${plugin##*/}"
+    if ! grep -q "${plugin_basename}" "${DS_PLUGINS_INSTALLED_FILE}" && dsplugin.sh "${plugin}" ; then
+      dsload
+    fi
+  done
 }
