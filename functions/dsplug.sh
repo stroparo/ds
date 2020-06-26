35f0fd34d5f63faffc2e148c3011b0430e6fec48
Cristian Stroparo <stroparo@outlook.com>
2020-06-26 18:21:39 -0300

    do not load ds if plugin was already installed (still letting dsplugin echo it was already installed)

--- functions/dsplug.sh
+++ functions/dsplug.sh
@@ -2,7 +2,13 @@ dsplug () {
   for plugin in "$@" ; do
     plugin_basename="${plugin%.git}"
     plugin_basename="${plugin##*/}"
-    if ! grep -q "${plugin_basename}" "${DS_PLUGINS_INSTALLED_FILE}" && dsplugin.sh "${plugin}" ; then
+
+    was_already_installed=false
+    if grep -q "${plugin_basename}" "${DS_PLUGINS_INSTALLED_FILE}" ; then
+      was_already_installed=true
+    fi
+
+    if dsplugin.sh "${plugin}" && ! ${was_already_installed} ; then
       dsload
     fi
   done
