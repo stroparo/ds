43d840100323bdb7ce49386ac79163a8dc138cab
Cristian Stroparo <stroparo@outlook.com>
2020-12-09 10:12:39 -0300

    Add rmfd function that removes active files descriptors of a file

---
+++ functions/rmfd.sh
@@ -0,0 +1,18 @@
+rmfd () {
+  for expr in "$@" ; do
+    pids="$(lsof +L1 | grep "${expr}" | fgrep '(deleted)' | awk '{ print $2; }')"
+    for pid in $(echo ${pids}) ; do
+      (
+        cd /proc/${pid}/fd
+        descriptors=$(ls -l | grep "${expr}" | fgrep '(deleted)' | awk '{ print $9; }')
+        for descriptor in $(echo ${descriptors}) ; do
+          if [ -e ${descriptor} ]; then
+            ls -l ${PWD}/${descriptor}
+            sudo su -c "> ${PWD}/${descriptor}"
+            ls -l ${PWD}/${descriptor}
+          fi
+        done
+      )
+    done
+  done
+}
