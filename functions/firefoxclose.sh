7913a95dc407fc1596be77477d8df9c6859ca54a
Cristian Stroparo <stroparo@outlook.com>
2020-12-13 18:17:22 -0300

    Refactor firefoxclose and update timeouts

--- functions/firefoxclose.sh
+++ functions/firefoxclose.sh
@@ -1,28 +1,30 @@
 firefoxclose () {
+  typeset timeout=2
+  typeset timeoutsmall=0.5
+
   if ! which xdotool >/dev/null 2>&1; then
     killall -HUP firefox
-    sleep 2
-    pidof firefox >/dev/null 2>&1
+    sleep ${timeout}
+    ! pidof firefox >/dev/null 2>&1
     return $?
   fi
 
-  FFWID=$(xdotool search --name "Mozilla Firefox" | head -1)
+  FFWID="$(xdotool search --name "Mozilla Firefox" | head -1)"
   xdotool windowactivate --sync $FFWID
 
   # https://support.mozilla.org/en-US/kb/keyboard-shortcuts-perform-firefox-tasks-quickly
   xdotool key --clearmodifiers ctrl+q
-
-  sleep 1
+  sleep ${timeoutsmall}
 
   # Confirmation window, if any:
-  CWID=$(xdotool search --name "close tabs")
+  CWID="$(xdotool search --name "close tabs")"
   if [ $? -ne 0 ] && ! pidof firefox >/dev/null 2>&1; then
     return 0
   fi
   xdotool windowactivate --sync $CWID
   xdotool key --clearmodifiers Return
+  sleep ${timeout}
 
-  sleep 2
   ! pidof firefox >/dev/null 2>&1
   return $?
 }
