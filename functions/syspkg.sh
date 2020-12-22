7eef1f5479ff66dd0a639093b5dcd887ff3cf0fd
Cristian Stroparo <stroparo@outlook.com>
2020-12-22 00:19:33 -0300

    Add system packaging functions installpkgs, installpkgsepel and installrepoepel

--- functions/syspkg.sh
+++ functions/syspkg.sh
@@ -1,3 +1,40 @@
+installpkgs () {
+  typeset timestamp="$(date '+%Y%m%d-%OH%OM%OS')"
+
+  echo "${PROGNAME:-installpkgs()}: INFO: Installing packages..."
+  if ! (sudo "${INSTPROG}" install -y "$@" 2>&1 | tee "/tmp/pkg-install-${timestamp}.log") ; then
+    echo "${PROGNAME:-installpkgs()}: WARN: There was an error installing packages - see '/tmp/pkg-install-${timestamp}.log'." 1>&2
+  fi
+}
+
+
+installpkgsepel () {
+  if ! _is_el_family ; then return ; fi
+
+  typeset timestamp="$(date '+%Y%m%d-%OH%OM%OS')"
+
+  echo "${PROGNAME:-installpkgsepel()}: INFO: Installing EPEL packages..."
+  if ! (sudo "${INSTPROG}" install -y --enablerepo=epel "$@" 2>&1 | tee "/tmp/pkg-install-epel-${timestamp}.log") ; then
+    echo "${PROGNAME:-installpkgsepel()}: WARN: There was an error installing packages - see '/tmp/pkg-install-epel-${timestamp}.log'." 1>&2
+  fi
+}
+
+
+installrepoepel () {
+  # Package indexing setup
+  sudo yum makecache fast
+
+  if ! _is_fedora ; then
+    echo "PROGNAME: INFO: EPEL - Extra Packages for Enterprise Linux..." # https://fedoraproject.org/wiki/EPEL
+    if _is_el7 ; then
+      installpkgs https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+    else
+      installpkgs epel-release.noarch
+    fi
+  fi
+}
+
+
 validpkgshelper () {
   typeset pkg_list_filename="${1}"
   typeset pkg_list="$(sed -e 's/ *#.*$//' "${pkg_list_filename}" | tr '\n' ' ')"
@@ -11,17 +48,20 @@ validpkgshelper () {
   | tr -s '\n' ' '
 }
 
+
 validpkgsapt () {
   typeset pkg_list_filename="${1}"
   typeset cmd="apt-cache show"
   validpkgshelper "${pkg_list_filename}" "${cmd}"
 }
 
+
 validpkgsrpm () {
   typeset pkg_list_filename="${1}"
   typeset cmd="yum info"
   validpkgshelper "${pkg_list_filename}" "${cmd}"
 }
+
 
 validpkgs () {
   if _is_debian_family ; then
