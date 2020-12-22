73cba91e3c0d6021e2f02a8523663c4822a7ef1e
Cristian Stroparo <stroparo@outlook.com>
2020-12-21 23:46:17 -0300

    Update ds10varinstprog.sh to use apt-get instead of apt (apt-get still better for scripts?)

--- ds10varinstprog.sh
+++ ds10varinstprog.sh
@@ -1,5 +1,5 @@
-export APTPROG=apt
+export APTPROG=apt-get
 export RPMPROG=yum
 export RPMGROUP="yum groupinstall"
-if [ -e /sbin/dnf ] ; then export RPMPROG=dnf ; export RPMGROUP="dnf group install"; fi
-if [ -e /sbin/apt ] ; then export INSTPROG="$APTPROG" ; else export INSTPROG="$RPMPROG"; fi
+if which dnf >/dev/null 2>&1 ; then export RPMPROG=dnf ; export RPMGROUP="dnf group install"; fi
+if which "$APTPROG" >/dev/null 2>&1 ; then export INSTPROG="$APTPROG" ; else export INSTPROG="$RPMPROG"; fi
