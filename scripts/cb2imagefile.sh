a7d1e10efbf4445ed3d0e81ab638797adf9fd792
Cristian Stroparo <stroparo@outlook.com>
2020-12-22 09:31:02 -0300

    Update cb2imagefile.sh to accept directory argument

--- scripts/cb2imagefile.sh
+++ scripts/cb2imagefile.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
 PROGNAME="cb2imagefile.sh"
-: ${DS_DIR_IMAGES:=${HOME}/Pictures} ; export DS_DIR_IMAGES
 
 if ! (xclip -selection clipboard -t TARGETS -o | grep -i -q 'image/png') ; then
   echo "${PROGNAME:+$PROGNAME: }SKIP: There is no image in the clipboard." 1>&2
   exit
 fi
 
-if [ ! -d "${DS_DIR_IMAGES}" ] ; then mkdir -p "${DS_DIR_IMAGES}" >/dev/null 2>&1 ; fi
+if [ -d "${1}" ] ; then
+  FINAL_DIR="${1}"
+else
+  FINAL_DIR="${DS_DIR_IMAGES:-${HOME}/Pictures}"
+fi
+if [ ! -d "${FINAL_DIR}" ] ; then
+  mkdir -p "${FINAL_DIR}" >/dev/null 2>&1
+fi
 
-xclip -selection clipboard -t "image/png" -o > "${DS_DIR_IMAGES}/clipboard-$(date '+%Y%m%d').png"
+xclip -selection clipboard -t "image/png" -o > "${FINAL_DIR}/clipboard-$(date '+%Y%m%d').png"
 
 if which "${FILEMGR:-thunar}" >/dev/null 2>&1 ; then
-  "${FILEMGR:-thunar}" "${DS_DIR_IMAGES}"
+  "${FILEMGR:-thunar}" "${FINAL_DIR}"
 fi
