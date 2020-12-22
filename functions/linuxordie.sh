13732a6b30ffb8951c1ee97ac685059c12ce245b
Cristian Stroparo <stroparo@outlook.com>
2020-12-21 23:50:54 -0300

    Fix shell negation in zsh for linuxordie()

--- functions/linuxordie.sh
+++ functions/linuxordie.sh
@@ -1,5 +1,5 @@
 linuxordie () {
-  if !(uname -a | grep -i -q linux 2>/dev/null) ; then
+  if ! (uname -a | grep -i -q linux 2>/dev/null) ; then
     echo "${PROGNAME:+$PROGNAME: }SKIP: Only Linux is supported." 1>&2
     exit
   fi
