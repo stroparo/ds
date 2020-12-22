d8955d84d35106237a86d944f337e827dfde69af
Cristian Stroparo <stroparo@outlook.com>
2020-12-21 22:59:59 -0300

    Put back VISUAL editor window check in vs()

--- functions/dsdevinteractive.sh
+++ functions/dsdevinteractive.sh
@@ -10,8 +10,23 @@ vs () {
     shift
   fi
 
-  for vs_dir in "${first}" "$@" ; do
-    if [ -z "${vs_dir}" ] ; then continue ; fi
-    "${VISUAL}" "${vs_dir}"
-  done
+  if ! which xdotool >/dev/null 2>&1 && which apt-get >/dev/null 2>&1; then
+    sudo apt-get update && sudo apt-get install xdotool
+  fi
+
+  if ! which xdotool >/dev/null 2>&1 ; then
+    "${VISUAL}" "${first}" "$@"
+  else
+    for vs_dir in "${first}" "$@" ; do
+      if [ -z "${vs_dir}" ] ; then continue ; fi
+      # Here assuming the editor puts the dir. basename at the window title:
+      WINDOW_ID="$(xdotool search --name "$(basename "${vs_dir}").*(atom|subl|//vscode|webstorm)" | head -1)"
+      if [ -z "${WINDOW_ID}" ] ; then
+        # Here assuming the GUI editor invoked puts itself in background right away:
+        "${VISUAL}" "${vs_dir}"
+      else
+        echo "vs(): SKIP: Window already opened for this dir.'s basename ('$(basename "${vs_dir}")')." 1>&2
+      fi
+    done
+  fi
 }
