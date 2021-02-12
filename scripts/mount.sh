e045d80e08cb8cb84679fd429883c5164d163128
Cristian Stroparo <stroparo@outlook.com>
2021-02-11 23:10:54 -0300

    Fix mount.sh

--- scripts/mount.sh
+++ scripts/mount.sh
@@ -14,10 +14,10 @@ _mount () {
 
     if ! grep -q "${mount_point}" /etc/fstab ; then
       echo "${PROGNAME}: SKIP: Mount point '${mount_point}' not in fstab." 1>&2
-      return
+      continue
     elif grep -q "${mount_point}" /etc/mtab ; then
       echo "${PROGNAME}: SKIP: Already mounted '${mount_point}'." 1>&2
-      return
+      continue
     else
       if ! sudo mount "${mount_point}" ; then
         result=1
