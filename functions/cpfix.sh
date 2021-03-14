7de96f0ea4922a3d0250e71fc164c9c1ed8db5a7
Cristian Stroparo <stroparo@outlook.com>
2021-03-14 15:35:12 -0300

    Add 2nd usage form to cpfix()

--- functions/cpfix.sh
+++ functions/cpfix.sh
@@ -6,13 +6,22 @@ cpfix () {
 
   # TODO test
 
-  typeset usage="{fixed-filename} {target-tree-path} [target-filename-relative-to-tree=bin/start.bat]"
+  typeset usage="
+USAGE
+cpfix {fixed-filename} {target-tree-path|target-filename} [target-filename-relative-to-tree=bin/start.bat]
+
+EXAMPLES
+cpfix start.bat target/dir bin/start.bat
+cpfix start.bat target/dir/bin/start.bat
+
+The 2nd form only works when the file already exists.
+"
 
   typeset filename_fixed="${1}"
   typeset target_tree_path="${2:-${HOME}/opt/dummy_target_tree_path}"
   typeset target_rel_filename="${3:-bin/start.bat}"
   typeset target_filename="${target_tree_path}/${target_rel_filename}"
-  typeset target_backup_filename="${target_filename}.original-$(%Y-%m-%d-%OH-%OM-%OS)"
+  typeset target_backup_filename
 
   if [ ! -f "${filename_fixed}" ] ; then
     echo "${PROGNAME:+$PROGNAME: }INFO: Usage: ${usage}" 1>&2
@@ -20,14 +29,21 @@ cpfix () {
     return 1
   fi
 
-  if [ ! -d "${target_tree_path}" ] ; then
+  if [ ! -e "${target_tree_path}" ] ; then
     echo "${PROGNAME:+$PROGNAME: }INFO: Usage: ${usage}" 1>&2
-    echo "${PROGNAME:+$PROGNAME: }FATAL: No target_tree_path (${target_tree_path}) available." 1>&2
+    echo "${PROGNAME:+$PROGNAME: }FATAL: No target-tree-path|target-filename (${target_tree_path}) available." 1>&2
     return 1
   fi
 
-  cp -f -v "${target_filename}" "${target_backup_filename}"
-  if [ -e "${target_backup_filename}" ] ; then
-    cp -f -v "${filename_fixed}" "${target_filename}"
+  if [ -f "${target_tree_path}" ] ; then
+    target_filename="${target_tree_path}"
   fi
+  target_backup_filename="${target_filename}.original-$(%Y-%m-%d-%OH-%OM-%OS)"
+
+  cp -f -v "${target_filename}" "${target_backup_filename}"
+  if [ ! -f "${target_backup_filename}" ] ; then
+    echo "${PROGNAME:+$PROGNAME: }FATAL: Backup failed." 1>&2
+    return 1
+  fi
+  cp -f -v "${filename_fixed}" "${target_filename}"
 }
