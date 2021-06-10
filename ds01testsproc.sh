a152ab0dd26f0459e032111cb6094b34068edd26
Cristian Stroparo <stroparo@outlook.com>
2021-06-10 17:04:58 -0300

    Fix testing function

--- ds01testsproc.sh
+++ ds01testsproc.sh
@@ -1,2 +1,2 @@
-_is_vnc_active () { [ 0 -lt "$(ps -ef | grep -w '/usr/bin/[^ ]*vnc' | grep -v grep | wc -l)" ] ; }
-_is_vnc_inactive () { [ 0 -eq "$(ps -ef | grep -w '/usr/bin/[^ ]*vnc' | grep -v grep | wc -l)" ] ; }
+_is_vnc_active () { test 0 -lt "$(ps -ef | grep -w '/usr/bin/[^ ]*vnc' | grep -v grep | wc -l)" ; }
+_is_vnc_inactive () { test 0 -eq "$(ps -ef | grep -w '/usr/bin/[^ ]*vnc' | grep -v grep | wc -l)" ; }
