bea9d8f85abf818e27728c3f5d001f10997f9f6b
Cristian Stroparo <stroparo@outlook.com>
2021-10-11 21:03:06 -0300

    Rewrite _up function with my own logic, not a 3rd party gist anymore

--- functions/up.sh
+++ functions/up.sh
@@ -1,16 +1,18 @@
-# Based on the up function from https://github.com/PogiNate/dots/commit/e13a056361f2b559d501eb19c6079b05e4510e64#diff-033f192d54c92104ab10fd785a61f24cR17
 
 _up () {
-	typeset rx updir
-	rx=$(ruby -e "print '$1'.gsub(/\s+/,'').split('').join('.*?')")
-	updir=`echo $PWD | ruby -e "print STDIN.read.sub(/(.*\/${rx}[^\/]*\/).*/i,'\1')"`
-	echo -n "$updir"
+	typeset searchterm="${1}"
+	typeset submatch finalmatch
+	submatch="$(echo "${PWD%}" | grep -o "^.*${searchterm}")"
+	finalmatch="$(echo "${PWD%}" | grep -o "^${submatch:-.*}[^/]*")"
+	echo -n "${finalmatch}"
 }
+
 
 up () {
 	if [ $# -eq 0 ]; then
-		echo "up: traverses up the current working directory to first match and cds to it"
-		echo "You need an argument"
+		echo "up: INFO: traverses up the current working directory to first match and cds to it"
+		echo "up: WARN: You need an argument"
+		return
 	else
 		cd $(_up "$@")
 	fi
